e17ef6ead67f84971c37bf8fb3e2be69a84bcbe8
Brendan Sudol <brendan.sudol@gsa.gov>
2016-12-07 15:14:49 -0500

    humble beginnings...

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => (
   <nav className='py2 bg-blue white'>
     <div className='container'>
-      <h2 className='m0 h5 caps center'>Crime Data Explorer</h2>
+      <h2 className='m0 h5 caps'>Crime Data Explorer</h2>
     </div>
   </nav>
 )
