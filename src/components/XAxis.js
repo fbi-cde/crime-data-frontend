bac41beef2ed432b2771444e23b1577a659171f3
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-12-09 02:00:35 -0500

    <XAxis /> supports an ordinal scale
    For bar charts, we want to use range bands for a better
    data display, so the x axis component needs to be able
    handle a different sort of scale.

--- src/components/XAxis.js
+++ src/components/XAxis.js
@@ -10,10 +10,16 @@ const XAxis = ({
   const [range0, range1, k] = [range[0] + 0.5, range[range.length - 1] + 0.5, 1]
   const domain = `M${range0},${k * tickSizeOuter}V0.5H${range1}V${k * tickSizeOuter}`
 
-  const format = scale.tickFormat ? scale.tickFormat.apply(scale) : x => x
-  const values = scale.ticks(tickCt)
+  const format = scale.tickFormat ? scale.tickFormat.apply(scale) : x => String(x)
+  const values = scale.ticks ? scale.ticks(tickCt) : scale.domain()
+
   const ticks = values.map((v, i) => {
-    const pos = scale(v)
+    let pos = scale(v)
+
+    if (typeof scale.bandwidth === 'function') {
+      pos = scale(v) + (scale.bandwidth() / 2)
+      pos -= scale.padding()
+    }
 
     return (
       <g key={i} transform={`translate(${pos}, 0)`} className='tick'>
