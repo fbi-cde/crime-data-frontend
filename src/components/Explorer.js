59646955c8fc2d8aea07dd94652702dbd7c486d8
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-12-14 17:11:49 -0500

    Add basic text to <Explorer />

--- src/components/Explorer.js
+++ src/components/Explorer.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import Breadcrumbs from './Breadcrumbs'
+import Term from './Term'
 
 const Explorer = props => {
   const state = props.params.state
 
   return (
     <div className='container'>
-      <div className='border-bottom clearfix pb2'>
+      <div className='border-bottom clearfix pb2 mb2'>
         <Breadcrumbs {...props} />
         <span className='sm-col flex flex-center'>
           <h1 className='py0 my0 titlecase'>{ state }</h1>
@@ -16,6 +17,22 @@ const Explorer = props => {
           <a className='btn'>Download this data</a>
           <a className='btn'>Share this page</a>
         </span>
+      </div>
+      <div className='clearfix'>
+        <div className='sm-col sm-col-8'>
+          <p className='bold'>
+            Incidents of <Term>rape</Term> are on the
+            rise in { state }, but lower than 5 or 10 years ago.
+          </p>
+          <p>
+            Ohio's rape rate surpassed that of the U.S. in 1985, and peaked
+            in 1991, with a rate of over 52 incidents per 100,000
+            people.<sup>1</sup>
+          </p>
+        </div>
+        <div className='sm-col-right sm-col-4'>
+          <p>Census block will go here</p>
+        </div>
       </div>
     </div>
   )
