c090f34bceb931b95029e9643bd06da228410c22
Brendan Sudol <brendan.sudol@gsa.gov>
2016-12-14 14:21:01 -0500

    indivual state svg component

--- src/components/Sandbox.js
+++ src/components/Sandbox.js
@@ -6,12 +6,19 @@ import Glossary from './Glossary'
 import Header from './Header'
 import TimeChart from './TimeChart'
 
+import StateSvg from './StateSvg'
+
 import { timeData, mapData } from '../util/data'
 
 const Sandbox = () => (
   <div>
     <Header />
     <div className='p2 container'>
+      <div className='mb3'>
+        <StateSvg state='dc' color='steelblue' />
+        <StateSvg state='ca' color='tomato' />
+        <StateSvg state='ny' color='springgreen' />
+      </div>
       <div className='mb3'>
         <Cartogram data={mapData} />
       </div>
