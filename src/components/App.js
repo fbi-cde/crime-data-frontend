c048c08508a49ff02ce3f9bcb6d3028c9f49c8ea
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-12-15 18:58:33 -0500

    Use react-redux to pass app state to <App />
    As the main container object, we want to get <App /> access to both the
    state object and the dispatch function. We can do this using connect()
    from react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,36 @@
+/* eslint react/forbid-prop-types: 0 */
+
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Footer from './Footer'
 import Glossary from './Glossary'
 import Navigation from './Navigation'
 
-const App = props => (
-  <div className='site'>
-    <div className='site-wrap'>
-      <Navigation />
-      <main className='container'>
-        { props.children }
-      </main>
-    </div>
-    <Glossary />
-    <Footer />
-  </div>
-)
+const App = props => {
+  const appState = props.appState
+  const dispatch = props.dispatch
 
-export default App
+  return (
+    <div className='site'>
+      <div className='site-wrap'>
+        <Navigation />
+        <main className='container'>
+          {props.children}
+        </main>
+      </div>
+      <Glossary />
+      <Footer />
+    </div>
+  )
+}
+
+App.propTypes = {
+  appState: React.PropTypes.object,
+  dispatch: React.PropTypes.func,
+}
+
+const mapStateToProps = state => ({ appState: state })
+const mapDispatchToProps = dispatch => ({ dispatch })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
