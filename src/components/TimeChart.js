c91319e54f7bb1bf4a1c9fdb29a91c4392d5929f
Brendan Sudol <brendan.sudol@gsa.gov>
2016-12-16 10:43:20 -0500

    multiple series in line chart

--- src/components/TimeChart.js
+++ src/components/TimeChart.js
@@ -1,5 +1,8 @@
-import { extent } from 'd3-array'
-import { scaleLinear, scaleTime } from 'd3-scale'
+import { extent, max } from 'd3-array'
+import {
+  scaleLinear, scaleOrdinal,
+  scaleTime, schemeCategory20c,
+} from 'd3-scale'
 import { line } from 'd3-shape'
 import { timeParse } from 'd3-time-format'
 import React from 'react'
@@ -11,19 +14,35 @@ const TimeChart = ({
   size = { width: 600, height: 400 },
   margin = { top: 20, right: 20, bottom: 30, left: 50 },
   data,
+  keys,
 }) => {
   const width = size.width - margin.left - margin.right
   const height = size.height - margin.top - margin.bottom
 
+  const color = scaleOrdinal(schemeCategory20c);
   const parse = timeParse('%Y-%m-%d')
-  const dataClean = data.map(d => ({ date: parse(d[0]), value: +d[1] }))
+
+  // parse date, ensure all key cols are numbers
+  const dataClean = data.map(d => (
+    Object.assign({},
+      { date: parse(d.date) },
+      ...keys.map(id => ({ [id]: +d[id] })),
+    )
+  ))
+
+  // nest data by key, standardize naming
+  const dataByKey = keys.map(id => ({
+    id: id.toLowerCase().replace(/\s/g, '_'),
+    name: id,
+    values: dataClean.map(d => ({ date: d.date, value: d[id] })),
+  }))
 
   const x = scaleTime()
       .domain(extent(dataClean, d => d.date))
       .range([0, width])
 
   const y = scaleLinear()
-      .domain(extent(dataClean, d => d.value))
+      .domain([0, max(dataByKey, d => max(d.values, v => v.value))])
       .range([height, 0])
 
   const l = line()
@@ -35,20 +54,26 @@ const TimeChart = ({
       <g transform={`translate(${margin.left}, ${margin.top})`}>
         <XAxis scale={x} height={height} tickCt={4} />
         <YAxis scale={y} />
-        <path
-          d={l(dataClean)}
-          fill='none'
-          stroke='tomato'
-          strokeWidth={3}
-        />
-        {dataClean.map((d, i) => (
-          <circle
-            key={i}
-            cx={x(d.date)}
-            cy={y(d.value)}
-            fill='tomato'
-            r={5}
-          />
+        {dataByKey.map((d, i) => (
+          <g key={i} className={`series series-${d.id}`}>
+            <path
+              d={l(d.values)}
+              fill='none'
+              stroke={color(d.id)}
+              strokeWidth='3'
+            />
+            {d.values.map((v, j) => (
+              <circle
+                key={j}
+                cx={x(v.date)}
+                cy={y(v.value)}
+                fill={color(d.id)}
+                r='6'
+                stroke='#fff'
+                strokeWidth='3'
+              />
+            ))}
+          </g>
         ))}
       </g>
     </svg>
@@ -56,7 +81,7 @@ const TimeChart = ({
 }
 
 TimeChart.propTypes = {
-  data: React.PropTypes.arrayOf(React.PropTypes.array).isRequired,
+  data: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
 }
 
 export default TimeChart
--- test/components/TimeChart.test.js
+++ test/components/TimeChart.test.js
@@ -4,18 +4,27 @@ import { shallow } from 'enzyme';
 import TimeChart from '../../src/components/TimeChart';
 
 describe('TimeChart', () => {
-  const data = [['2016-01-01', 5], ['2016-01-02', 7]]
+  const data = [
+    { date: '2016-01-01', foo: 5, bar: 7 },
+    { date: '2016-01-02', foo: 7, bar: 2 },
+    { date: '2016-01-03', foo: 4, bar: 4 },
+  ]
+  const keys = ['foo', 'bar']
   let chart
 
   beforeEach(() => {
-    chart = shallow(<TimeChart data={data} />)
+    chart = shallow(<TimeChart data={data} keys={keys} />)
   });
 
   it('TimeChart renders svg', () => {
     expect(chart.find('svg').length).toEqual(1)
   })
 
-  it('TimeChart has a circle element for each data point', () => {
-    expect(chart.find('circle').length).toEqual(data.length)
+  it('TimeChart has a line (<path>) for each series', () => {
+    expect(chart.find('.series path').length).toEqual(keys.length)
+  })
+
+  it('TimeChart has a <circle> for each data point', () => {
+    expect(chart.find('.series-foo circle').length).toEqual(data.length)
   })
 })
