52aaa6c0208f9ae78b09d33d9a2c0692732c1a40
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-12-16 01:02:11 -0500

    More small style changes

--- src/actions/glossaryActions.js
+++ src/actions/glossaryActions.js
@@ -4,21 +4,15 @@ import {
   GLOSSARY_SHOW_TERM,
 } from '../constants'
 
-const hide = () => ({
+export const hide = () => ({
   type: GLOSSARY_HIDE,
 })
 
-const show = () => ({
+export const show = () => ({
   type: GLOSSARY_SHOW,
 })
 
-const showTerm = term => ({
+export const showTerm = term => ({
   type: GLOSSARY_SHOW_TERM,
   term,
 })
-
-export {
-  hide,
-  show,
-  showTerm,
-}
