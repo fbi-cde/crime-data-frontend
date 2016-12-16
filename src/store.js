1e033bb28764c0e84d5eca1ded60897dd8f4c9d9
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-12-16 01:10:31 -0500

    Add basic logging middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,16 @@
-import { combineReducers, createStore } from 'redux'
+/* eslint no-console: 0 */
+
+import { combineReducers, createStore, applyMiddleware } from 'redux'
 import glossary from './reducers/glossaryReducer'
 
+const logger = store => next => action => {
+  console.log('::dispatching::', action)
+  const result = next(action)
+  console.log('::next state::', store.getState())
+  return result
+}
+
 const reducer = combineReducers({ glossary })
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(logger))
 
 export default store
