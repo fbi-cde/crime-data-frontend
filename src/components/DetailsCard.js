9d5aa041f329c7d00c5e9fba9272b1a18620e1bc
Brendan Sudol <brendan.sudol@gsa.gov>
2016-12-19 18:06:41 -0500

    add table, progress bar unit

--- src/components/DetailsCard.js
+++ src/components/DetailsCard.js
@@ -1,13 +1,44 @@
+import { format } from 'd3-format'
 import React from 'react'
 
 const DetailsCard = ({
   data,
   title,
-}) => (
-  <div className='p2 bg-silver'>
-    <h2 className='mt0'>{title}</h2>
-    {JSON.stringify(data)}
-  </div>
-)
+}) => {
+  const colWidths = ['3', '2', '4', '3']
+  const formatPercent = format('.0%')
+  const formatNumber = format(',')
+
+  return (
+    <div className='p2 border rounded'>
+      <h2 className='mt0'>{title}</h2>
+      <div className='mb1 h6 blue caps bold right-align'>Incidents</div>
+      <table>
+        <thead>
+          {colWidths.map((c, i) => <th key={i} className={`col-${c}`} />)}
+        </thead>
+        <tbody>
+          {data.map((d, i) => (
+            <tr key={i}>
+              <td>
+                <div className='progress-bar my1'>
+                  <span className='rtl' style={{ width: `${d.percent * 100}%` }} />
+                </div>
+              </td>
+              <td className='right-align'>{formatPercent(d.percent)}</td>
+              <td className='px2'>{d.key}</td>
+              <td className='right-align'>{formatNumber(d.count)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+DetailsCard.propTypes = {
+  data: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+  title: React.PropTypes.string.isRequired,
+}
 
 export default DetailsCard
