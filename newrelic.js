b0c883212e16c05f3472f72a54a313931a4e9c90
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2016-12-19 22:43:17 -0500

    Add new relic for production server monitoring (#70)
    * Add new relic
    
    * Ignore new relic log

---
+++ newrelic.js
@@ -0,0 +1,18 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+const cfenv = require('cfenv')
+
+const env = cfenv.getAppEnv()
+const credService = env.getService('crime-data-api-creds') || { credentials: {} }
+
+exports.config = {
+  app_name: ['crime-data-explorer'],
+  license_key: credService.credentials.NEW_RELIC_EXPLORER_KEY,
+  logging: {
+    level: 'info',
+  },
+}
