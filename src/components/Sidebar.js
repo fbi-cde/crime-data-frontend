6f59305bece39dd676cde46ca349ec8987c8167d
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-12-20 19:55:43 -0500

    Use <Filter /> and <FilterField /> in <Sidebar />
    Try out using the filter and filter field components in the sidebar,
    passing in a wrapped dispatch function.
    
    Add a debouncer to keep the changes from being too rapid on the time
    fields

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,38 +1,75 @@
+import debounce from 'lodash.debounce'
 import React from 'react'
 
 import Filter from './Filter'
+import FilterField from './FilterField'
 import { updateFilter } from '../actions/filterActions'
 
-const eventAction = e => updateFilter(e)
-
-const filters = [
-  {
-    label: 'Crime type',
-    type: 'select',
-    options: ['Yo', 'Dude', 'Hey'],
-  },
-  {
-    label: 'States',
-    type: 'checkbox',
-    options: ['AK', 'CA', 'IL', 'NY'],
-  },
+const propertyCrime = [
+  'All property crime',
+  'Burglary',
 ]
 
-const Sidebar = ({ dispatch }) => (
-  <nav className='site-sidebar bg-silver'>
-    <div className='p2 sm-p3'>
-      <h2 className='mt0'>Sidebar</h2>
-    </div>
-    { filters.map((f, i) => (
-      <Filter
-        key={i}
-        onChange={e => { dispatch(eventAction(e)) }}
-        {...f}
-      />
-    ))}
+const usStates = ['AZ', 'CA', 'MT', 'NJ', 'PA', 'WV']
 
-  </nav>
-)
+const violentCrime = [
+  'All violent crime',
+  'Murder',
+  'Rape',
+  'Robbery',
+  'Aggravated assult',
+]
+
+const Sidebar = ({ dispatch }) => {
+  const handleChange = change => dispatch(updateFilter(change))
+  const debouncedHandleChange = debounce(handleChange, 800)
+  return (
+    <nav className='site-sidebar bg-silver'>
+      <div className='p2 sm-p3'>
+        <h2 className='mt0'>Sidebar</h2>
+        <Filter legend='Location' id='location'>
+          <FilterField
+            label='State'
+            onChange={handleChange}
+            options={usStates}
+            type='select'
+          />
+        </Filter>
+        <Filter legend='Time period' id='time-period'>
+          <FilterField
+            className='col col-5'
+            label='Time from'
+            onChange={debouncedHandleChange}
+            showLabel={false}
+            type='number'
+          />
+          <span className='align-middle col col-2 center'>to</span>
+          <FilterField
+            className='col col-5'
+            label='Time to'
+            onChange={debouncedHandleChange}
+            showLabel={false}
+            type='number'
+          />
+        </Filter>
+        <Filter legend='Type of crime' id='type-of-crime'>
+          <FilterField
+            label='Violent crime'
+            onChange={handleChange}
+            options={violentCrime}
+            type='radio'
+          />
+          <FilterField
+            label='Property crime'
+            onChange={handleChange}
+            options={propertyCrime}
+            type='radio'
+          />
+        </Filter>
+      </div>
+    </nav>
+  )
+}
 
 Sidebar.propTypes = {
   dispatch: React.PropTypes.func,
