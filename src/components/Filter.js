56efce3b90aa886789ce4d0932fc155604448c7a
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-12-20 19:54:17 -0500

    Refactor <Filter />
    Added a new component called <FilterField /> that represents the
    specific form field for a filter. Groups of filter fields can be used
    in a <Filter /> component, much like a <fieldset> can hold many input
    controls.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,89 +1,20 @@
 import React from 'react'
 
-import { slugify } from '../util/text'
-
-export default class Filter extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      expanded: true,
-      id: slugify(props.label),
-    }
-
-    this.getCheckboxValues = ::this.getCheckboxValues
-    this.toggle = ::this.toggle
-  }
-
-  getCheckboxValues() {
-    const id = `#${this.state.id}`
-    const type = '[type="checkbox"]'
-    const sel = document.querySelector(id).querySelectorAll(type)
-    const boxes = Array.prototype.slice.call(sel)
-    return boxes.filter(b => b.checked).map(b => b.value)
-  }
-
-  toggle() {
-    const { expanded } = this.state
-    this.setState({ expanded: !expanded })
-  }
-
-  render() {
-    const { label, onChange, options, type } = this.props
-    const { expanded, id } = this.state
-    let field
-    let fn
-
-    switch (type.toLowerCase()) {
-      case 'checkbox':
-        fn = () => onChange({ id, value: this.getCheckboxValues() })
-        field = (
-          <fieldset id={id} onChange={fn}>
-            {options.map((o, i) => (
-              <label key={i} htmlFor={`${id}[${slugify(o)}]`}>
-                <input
-                  type='checkbox'
-                  id={`${id}[${slugify(o)}]`}
-                  value={slugify(o)}
-                />
-                {o}
-              </label>
-            ))}
-          </fieldset>
-        )
-        break;
-      default:
-        fn = e => onChange({ id, value: e.target.value })
-        field = (
-          <select className='fit' id={id} onChange={fn}>
-            { options.map((o, i) => (
-              <option key={i}>{o}</option>
-            ))}
-          </select>
-        )
-    }
-    return (
-      <div className='filter flex flex-column px2' aria-expanded={expanded}>
-        <label
-          className='h3 block'
-          htmlFor={id}
-          onClick={() => this.toggle()}
-        >
-          {label}
-        </label>
-        {field}
-      </div>
-    )
-  }
-}
-
-Filter.defaultProps = {
-  type: 'select',
-  options: [],
-  onChange: () => {},
-}
+const Filter = ({ children, id, legend }) => (
+  <fieldset
+    id={id}
+    className='border-none mt2 pl0'
+  >
+    <legend className='border-bottom col-12 h3'>
+      {legend}
+    </legend>
+    { children }
+  </fieldset>
+)
 
 Filter.propTypes = {
-  type: React.PropTypes.string,
-  label: React.PropTypes.string.isRequired,
-  onChange: React.PropTypes.func,
+  id: React.PropTypes.string.isRequired,
+  legend: React.PropTypes.string.isRequired,
 }
+
+export default Filter
---
+++ src/components/FilterField.js
@@ -0,0 +1,98 @@
+import React from 'react'
+
+import { slugify } from '../util/text'
+
+export default class FilterField extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expanded: true,
+      id: slugify(props.label),
+    }
+
+    this.getCheckboxValues = ::this.getCheckboxValues
+    this.toggle = ::this.toggle
+  }
+
+  getCheckboxValues() {
+    const id = `#${this.state.id}`
+    const type = '[type="checkbox"]'
+    const sel = document.querySelector(id).querySelectorAll(type)
+    const boxes = Array.prototype.slice.call(sel)
+    return boxes.filter(b => b.checked).map(b => b.value)
+  }
+
+  toggle() {
+    const { expanded } = this.state
+    this.setState({ expanded: !expanded })
+  }
+
+  render() {
+    const { options } = this.props
+    const { expanded, id } = this.state
+    const fieldProps = {
+      id,
+      onChange: e => (
+        this.props.onChange({ id, value: slugify(e.target.value) })
+      ),
+    }
+    let field
+
+    switch (this.props.type.toLowerCase()) {
+      case 'select':
+        field = (
+          <select>
+            {options.map((o, i) => (<option key={i}>{o}</option>))}
+          </select>
+        )
+        break
+      case 'radio':
+        field = (
+          <fieldset className='border-none p0'>
+            {options.map((o, i) => (
+              <label className='block' key={i}>
+                <input
+                  className='mr1'
+                  name={`${id}`}
+                  type='radio'
+                  value={slugify(o)}
+                />
+                {o}
+              </label>
+            ))}
+          </fieldset>
+        )
+        break
+      default:
+        field = (<input type={this.props.type} />)
+    }
+    const labelClass = (this.props.showLabel) ? '' : 'display-none'
+
+    return (
+      <div
+        className={`flex flex-column ${this.props.className}`}
+        aria-expanded={expanded}
+      >
+        <label
+          className={`block bold border-bottom my1 ${labelClass}`}
+          htmlFor={id}
+        >
+          {this.props.label}
+        </label>
+        {React.cloneElement(field, fieldProps)}
+      </div>
+    )
+  }
+}
+
+FilterField.defaultProps = {
+  className: '',
+  showLabel: true,
+  type: 'text',
+}
+
+FilterField.propTypes = {
+  label: React.PropTypes.string.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+  type: React.PropTypes.string,
+}
