33328dd2047fe754b2ff0316969a333ffe4d6a4a
Brendan Sudol <brendansudol@gmail.com>
2016-12-29 13:52:44 -0500

    add basic table component / stylez (#83)

---
+++ src/components/Table.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Table = ({ cls, data, headings }) => (
+  <table className={`table ${cls}`}>
+    <thead>
+      <tr>
+        {headings.map((heading, i) => (
+          <th key={i}>{heading}</th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {data.map((row, i) => (
+        <tr key={i}>
+          {row.map((datum, j) => (
+            <td key={j}>{datum}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)
+
+Table.propTypes = {
+  cls: React.PropTypes.string,
+  data: React.PropTypes.arrayOf(React.PropTypes.arrayOf(React.PropTypes.string)),
+  headings: React.PropTypes.arrayOf(React.PropTypes.string),
+}
+
+Table.defaultProps = {
+  cls: '',
+  data: [],
+  headings: [],
+}
+
+export default Table
