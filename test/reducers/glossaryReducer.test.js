8967ec7b05dabf38e60ee2a3a7dea23740274378
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-12-30 15:55:49 -0500

    Reduce show term glossary action
    Handle the show term action to set the glossary to visible and to set
    the term to be shown.
    
    Use the spread syntax to reduce Object.assign repeated calls

--- test/reducers/glossaryReducer.test.js
+++ test/reducers/glossaryReducer.test.js
@@ -3,16 +3,16 @@
 import {
   GLOSSARY_HIDE,
   GLOSSARY_SHOW,
+  GLOSSARY_SHOW_TERM,
 } from '../../src/actions/actionTypes';
 
 import reducer from '../../src/reducers/glossaryReducer';
 
 describe('glossaryReducer', () => {
-  describe('initial state', () => {
-    it('should return isVisible: false', () => {
-      const initialState = reducer(undefined, { type: 'fake' })
-      expect(initialState.isVisible).toEqual(false)
-    })
+  it('it should return the initial state', () => {
+    const initialState = reducer(undefined, { type: 'fake' })
+    expect(initialState.isVisible).toEqual(false)
+    expect(initialState.term).toEqual(null)
   })
 
   describe('GLOSSARY_HIDE action type', () => {
@@ -28,6 +28,22 @@ describe('glossaryReducer', () => {
       const initialState = { isVisible: false }
       const actual = reducer(initialState, { type: GLOSSARY_SHOW })
       expect(actual.isVisible).toEqual(true)
+    })
+  })
+
+  describe('GLOSSARY_SHOW_TERM action type', () => {
+    it('should set isVisible to true', () => {
+      const initialState = { isVisible: false }
+      const action = { term: 'fake', type: GLOSSARY_SHOW_TERM }
+      const actual = reducer(initialState, action)
+      expect(actual.isVisible).toEqual(true)
+    })
+
+    it('should set term to action.term', () => {
+      const initialState = { isVisible: false }
+      const action = { term: 'fake', type: GLOSSARY_SHOW_TERM }
+      const actual = reducer(initialState, action)
+      expect(actual.term).toEqual(action.term)
     })
   })
 })
