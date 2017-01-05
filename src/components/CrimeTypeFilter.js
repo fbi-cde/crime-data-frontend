ce144d2b68a5e63df06d6a367b0384d403650113
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-05 14:26:23 -0500

    Ensure <CrimeTypeFilter /> properly renders with initial values

--- src/components/CrimeTypeFilter.js
+++ src/components/CrimeTypeFilter.js
@@ -12,14 +12,12 @@ const violentCrime = [
   'Murder',
   'Rape',
   'Robbery',
-  'Aggravated assult',
+  'Aggravated assualt',
 ]
 
 const CrimeTypeFilter = ({ onChange, selected }) => {
-  const handleChange = e => {
-    const value = slugify(e.target.value)
-    onChange({ id: 'state', value })
-  }
+  const handleChange = e => onChange({ crime: e.target.value })
+  const selectedSlug = slugify(selected)
   return (
     <div id='type-of-crime' className='mb3'>
       <div className='mb2 pb-tiny h5 caps bold border-bottom'>
@@ -27,10 +25,13 @@ const CrimeTypeFilter = ({ onChange, selected }) => {
       </div>
       <div className='mb2'>
         {violentCrime.map((o, i) => (
-          <label className='block' key={i} htmlFor='violent-crime'>
+          <label className='block' key={i} htmlFor={slugify(o)}>
             <input
+              checked={slugify(o) === slugify(selected)}
               className='mr1'
-              name='violent-crime'
+              id={slugify(o)}
+              name='crime'
+              onChange={handleChange}
               type='radio'
               value={slugify(o)}
             />
@@ -40,8 +41,16 @@ const CrimeTypeFilter = ({ onChange, selected }) => {
       </div>
       <div className='mb2'>
         {propertyCrime.map((o, i) => (
-          <label className='block' key={i} htmlFor='property-crime'>
-            <input className='mr1' type='radio' name='property-crime' value={slugify(o)} />
+          <label className='block' key={i} htmlFor={slugify(o)}>
+            <input
+              checked={slugify(o) === slugify(selected)}
+              className='mr1'
+              id={slugify(o)}
+              name='crime'
+              onChange={handleChange}
+              type='radio'
+              value={slugify(o)}
+            />
             {o}
           </label>
         ))}
