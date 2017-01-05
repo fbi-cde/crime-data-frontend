73858c2cff100026bb4bf50293780cad77b844d9
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-05 14:39:56 -0500

    Add location helpers to history util
    The history.push method expects react-router location objects, which
    are structured in a particular way. Added a helper function to take a
    current location object and return a new one that reflects the change.
    
    Added to the history utility since it is used where history.push would
    be and therefore is easy to import alongside

---
+++ test/util/history.test.js
@@ -0,0 +1,40 @@
+/* eslint no-undef: 0 */
+
+import history, { createNewLocation } from '../../src/util/history'
+
+describe('history utility', () => {
+  it('should export a react-router history singleton', () => {
+    expect(typeof history.createHref).toEqual('function')
+    expect(typeof history.push).toEqual('function')
+  })
+
+  describe('createNewLocation()', () => {
+    it('should change the place value in pathname', () => {
+      const change = { place: 'california' }
+      const location = { pathname: '/explorer/oregon/murder' }
+      const actual = createNewLocation({ change, location })
+      expect(actual.pathname).toEqual('/explorer/california/murder')
+    })
+
+    it('should change the crime value in pathname', () => {
+      const change = { crime: 'robbery' }
+      const location = { pathname: '/explorer/oregon/murder' }
+      const actual = createNewLocation({ change, location })
+      expect(actual.pathname).toEqual('/explorer/oregon/robbery')
+    })
+
+    it('should put other changes in the query object', () => {
+      const change = { timeTo: 2017 }
+      const location = { pathname: '/explorer/oregon/murder', query: { } }
+      const actual = createNewLocation({ change, location })
+      expect(actual.query.timeTo).toEqual(2017)
+    })
+
+    it('should ignore place and crime for the query', () => {
+      const change = { crime: 'robbery' }
+      const location = { pathname: '/explorer/oregon/murder', query: { } }
+      const actual = createNewLocation({ change, location })
+      expect(actual.query).toEqual({})
+    })
+  })
+})
