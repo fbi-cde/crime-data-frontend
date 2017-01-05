bca4ab0357ec9a11136a7a283275db7429bcc975
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-05 00:52:36 -0500

    Wrap history from react-router in util
    We can import either hashHistory or browserHistory from react-router,
    which are both singletons. However, since we use the history
    object/router in some actions, it is easy to just import the same
    history singleton from a wrapped util

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { hashHistory, IndexRedirect, Route, Router } from 'react-router'
+import { IndexRedirect, Route, Router } from 'react-router'
 
 import App from './components/App'
 import DownloadsAndDocs from './components/DownloadsAndDocs'
 import Explorer from './components/Explorer'
+import history from './util/history'
 import NotFound from './components/NotFound'
 import Sample from './components/Sample'
 import Sandbox from './components/Sandbox'
 
 const routes = (
-  <Router history={hashHistory}>
+  <Router history={history}>
     <Route path='/' component={App}>
       <IndexRedirect to='/explorer/ohio/murder' />
       <Route path='/downloads-and-docs' component={DownloadsAndDocs} />
