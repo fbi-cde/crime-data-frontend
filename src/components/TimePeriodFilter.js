424d8591c8788cd6a8fac9c5d6225ccbe880a2ba
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-05 14:26:06 -0500

    Ensure <TimePeriodFilter /> uses initial values

--- src/components/TimePeriodFilter.js
+++ src/components/TimePeriodFilter.js
@@ -1,38 +1,43 @@
 import React from 'react'
 
-import Filter from './Filter'
-import FilterField from './FilterField'
-
-const debouncedHandleChange = () => {}
-
 const minYear = 1960
 const maxYear = 2015
 
-const TimePeriodFilter = ({ from, to }) => (
-  <div id='time-period' className='mb3'>
-    <div className='mb2 pb-tiny h5 caps bold border-bottom'>
-      Time period
+const TimePeriodFilter = ({ onChange, timeFrom, timeTo }) => {
+  const handleChange = e => {
+    const { id, value } = e.target
+    onChange({ [id]: value })
+  }
+
+  return (
+    <div id='time-period' className='mb3'>
+      <div className='mb2 pb-tiny h5 caps bold border-bottom'>
+        Time period
+      </div>
+      <div className='clearfix'>
+        <label htmlFor='time-from'>Time from</label>
+        <input
+          className='block col-6 field'
+          type='number'
+          id='timeFrom'
+          min={minYear}
+          max={maxYear}
+          onChange={handleChange}
+          value={timeFrom}
+        />
+        <label htmlFor='time-to'>Time to</label>
+        <input
+          className='block col-6 field'
+          type='number'
+          id='timeTo'
+          min={minYear}
+          max={maxYear}
+          onChange={handleChange}
+          value={timeTo}
+        />
+      </div>
     </div>
-    <div className='clearfix'>
-      <label htmlFor='time-from'>Time from</label>
-      <input
-        className='block col-6 field'
-        type='number'
-        id='time-from'
-        min={minYear}
-        max={maxYear}
-        value={from}
-      />
-      <label htmlFor='time-to'>Time to</label>
-      <input
-        className='block col-6 field'
-        type='number'
-        id='time-to'
-        min={minYear}
-        max={maxYear}
-      />
-    </div>
-  </div>
-)
+  )
+}
 
 export default TimePeriodFilter
