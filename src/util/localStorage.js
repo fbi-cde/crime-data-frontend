72d2faf5288eb68a184d6f454e967a17676947be
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-08 23:10:13 -0500

    Add localStorage utility wrapper
    Add a utility function that wraps around localStorage, if it is
    available, and has two methods, with names lifted from the localStorage
    API.
    
    The wrapper utility functions do all the serialization, so getItem()
    returns a promise that resolves with the stored object, and
    setItem(key, value) takes an object as value.

---
+++ src/util/localStorage.js
@@ -0,0 +1,16 @@
+
+const getItem = key => {
+  if (!window.localStorage) return Promise.resolve(null)
+
+  const item = window.localStorage.getItem(key)
+  return Promise.resolve((item !== null) ? JSON.parse(item) : null)
+}
+
+const setItem = (key, value) => {
+  if (!window.localStorage) return Promise.resolve(null)
+
+  window.localStorage.setItem(key, JSON.stringify(value))
+  return Promise.resolve(true)
+}
+
+export default { getItem, setItem }
---
+++ test/util/localStorage.test.js
@@ -0,0 +1,74 @@
+/* eslint no-undef: 0 */
+
+import storage from '../../src/util/localStorage'
+
+const createStorageMock = () => {
+  const store = {}
+
+  return {
+    getItem: key => (key in store ? store[key] : null),
+    setItem: (key, value) => (store[key] = value || ''),
+  }
+}
+
+describe('localStorage utility', () => {
+  describe('without localStorage', () => {
+    beforeEach(() => {
+      window.localStorage = undefined
+    })
+
+    it('getItem() should return null', done => {
+      storage.getItem('fake-item').then(item => {
+        expect(item).toEqual(null)
+        done()
+      })
+    })
+
+    it('setItem() should return null', () => {
+      storage.setItem('fake-item', { fakeKey: 'fake-value' }).then(item => {
+        expect(item).toEqual(null)
+        done()
+      })
+    })
+  })
+
+  describe('with localStorage', () => {
+    beforeEach(() => {
+      window.localStorage = createStorageMock()
+    })
+
+    describe('getItem()', () => {
+      it('should return null if the key does not exist', done => {
+        const key = 'fake-item'
+
+        storage.getItem(key).then(item => {
+          expect(item).toEqual(null)
+          done()
+        })
+      })
+
+      it('should return the value if it exists', done => {
+        const key = 'fake-item'
+        const data = { fakeKey: 'fakeValue' }
+
+        window.localStorage.setItem(key, JSON.stringify(data))
+        storage.getItem(key).then(item => {
+          expect(item).toEqual(data)
+          done()
+        })
+      })
+    })
+
+    describe('setItem()', () => {
+      it('should return true if set was successful', done => {
+        const key = 'fake-item'
+        const data = { fakeKey: 'fakeValue' }
+
+        storage.setItem(key, data).then(item => {
+          expect(item).toEqual(true)
+          done()
+        })
+      })
+    })
+  })
+})
