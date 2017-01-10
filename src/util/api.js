046135c60807dd9c16150b3e527bb21adef50298
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-10 11:21:02 -0500

    Add API handlers for mocked data
    For now, adding API handling functions that can return data as we
    expect it so that we can iterate on the interface

--- src/util/api.js
+++ src/util/api.js
@@ -1,6 +1,10 @@
 /* eslint no-console: 0 */
 import { get, getAll } from './http'
 
+import incidents from '../../data/incidents.json'
+import nationalSummary from '../../data/national-summary.json'
+import stateSummary from '../../data/state-summary.json'
+
 const API = '/api'
 const ENDPOINTS = {
   incidents: `${API}/incidents`,
@@ -11,7 +15,13 @@ const getAllIncidents = params => (
 )
 
 const getIncidents = params => (
-  get(ENDPOINTS.incidents, params)
+  Promise.resolve({ results: incidents })
+  // get(ENDPOINTS.incidents, params)
 )
 
-export default { getAllIncidents, getIncidents }
+const getSummary = params => {
+  if (params.place) return Promise.resolve(stateSummary)
+  return Promise.resolve(nationalSummary)
+}
+
+export default { getAllIncidents, getIncidents, getSummary }
