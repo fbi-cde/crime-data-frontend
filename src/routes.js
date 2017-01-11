3b8c156b4c0b768d50807ebf1006688a46fa2033
Brendan Sudol <brendan.sudol@gsa.gov>
2017-01-11 13:10:07 -0500

    hotfix: remove sandbox from routes

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,6 @@ import Explorer from './components/Explorer'
 import history from './util/history'
 import NotFound from './components/NotFound'
 import Sample from './components/Sample'
-import Sandbox from './components/Sandbox'
 
 const routes = (
   <Router history={history}>
@@ -16,7 +15,6 @@ const routes = (
       <Route path='/downloads-and-docs' component={DownloadsAndDocs} />
       <Route path='/explorer/:place/:crime' component={Explorer} />
       <Route path='/sample' component={Sample} />
-      <Route path='/sandbox' component={Sandbox} />
       <Route path='/*' component={NotFound} />
     </Route>
   </Router>
