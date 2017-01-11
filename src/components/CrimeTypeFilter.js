d0743008e1fa0b574c2b68faf83dbdbf1a602cb9
Brendan Sudol <brendan.sudol@gsa.gov>
2017-01-10 23:33:47 -0500

    crime filter tweaks

--- src/components/CrimeTypeFilter.js
+++ src/components/CrimeTypeFilter.js
@@ -25,16 +25,21 @@ const violentCrime = [
   'Aggravated assualt',
 ]
 
-const CrimeTypeFilter = ({ onChange, selected }) => {
+const FilterGroup = ({ options, onChange, selected }) => {
   const handleChange = e => onChange({ crime: e.target.value })
+
   return (
-    <div id='type-of-crime' className='mb4'>
-      <h3 className='mt0 mb2 pb-tiny border-bottom'>Type of crime</h3>
-      <div className='mb2'>
-        {violentCrime.map((o, i) => (
-          <label className='block' key={i} htmlFor={slugify(o)}>
+    <div className='mb1 bg-light-blue rounded'>
+      {options.map((o, i) => {
+        const isActive = slugify(o) === slugify(selected)
+        return (
+          <label
+            key={i}
+            className={`py-tiny px2 block cursor-pointer ${isActive ? 'bg-darken-2' : ''}`}
+            htmlFor={slugify(o)}
+          >
             <input
-              checked={slugify(o) === slugify(selected)}
+              checked={isActive}
               className='mr1'
               id={slugify(o)}
               name='crime'
@@ -44,43 +49,35 @@ const CrimeTypeFilter = ({ onChange, selected }) => {
             />
             {o}
           </label>
-        ))}
-      </div>
-      <div className='mb2'>
-        {propertyCrime.map((o, i) => (
-          <label className='block' key={i} htmlFor={slugify(o)}>
-            <input
-              checked={slugify(o) === slugify(selected)}
-              className='mr1'
-              id={slugify(o)}
-              name='crime'
-              onChange={handleChange}
-              type='radio'
-              value={slugify(o)}
-            />
-            {o}
-          </label>
-        ))}
-      </div>
-      {otherCrime.map((o, i) => (
-        <div key={i} className='mb2'>
-          <label className='block' htmlFor={slugify(o)}>
-            <input
-              checked={slugify(o) === slugify(selected)}
-              className='mr1'
-              id={slugify(o)}
-              name='crime'
-              onChange={handleChange}
-              type='radio'
-              value={slugify(o)}
-            />
-            {o}
-          </label>
-        </div>
-      ))}
+        )
+      })}
     </div>
   )
 }
+
+const CrimeTypeFilter = ({ onChange, selected }) => (
+  <div id='type-of-crime' className='mb4'>
+    <h3 className='mt0 mb2 pb-tiny border-bottom'>Type of crime</h3>
+    <FilterGroup
+      options={violentCrime}
+      onChange={onChange}
+      selected={selected}
+    />
+    <FilterGroup
+      options={propertyCrime}
+      onChange={onChange}
+      selected={selected}
+    />
+    {otherCrime.map((o, i) => (
+      <FilterGroup
+        key={i}
+        options={[o]}
+        onChange={onChange}
+        selected={selected}
+      />
+    ))}
+  </div>
+)
 
 CrimeTypeFilter.defaultProps = {
   selected: '',
