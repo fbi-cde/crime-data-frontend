68b8be2ebc6c4d418fb9d4bb6d27b1b58330c860
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-13 18:40:41 -0500

    Show time filter error if the time range is too small
    This doesn't work super well, as it janks a little bit when an event
    changes. I think doing some debouncing could do the trick. I can do
    that in a different PR though.

--- src/components/TimePeriodFilter.js
+++ src/components/TimePeriodFilter.js
@@ -1,58 +1,89 @@
 import React from 'react'
 
-const minYear = 1960
-const maxYear = 2015
-const yearRange = 10
+class TimePeriodFilter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = ::this.handleChange
+    this.state = {
+      error: null,
+      minYear: 1960,
+      maxYear: 2015,
+      yearRange: 10,
+    }
+  }
 
-const TimePeriodFilter = ({ onChange, timeFrom, timeTo }) => {
-  const handleChange = e => {
+  handleChange(e) {
     const { id, value } = e.target
 
     const isFrom = id === 'timeFrom'
-    const otherId = isFrom ? 'timeTo' : 'timeFrom'
-    const otherVal = +value + (yearRange * (isFrom ? 1 : -1))
+    // const otherId = isFrom ? 'timeTo' : 'timeFrom'
+    // const otherVal = +value + (yearRange * (isFrom ? 1 : -1))
 
-    if (isFrom && otherVal > maxYear) return
-    else if (otherVal < minYear) return
+    const fromValue = (isFrom) ? +value : this.props.timeFrom
+    const toValue = (isFrom) ? this.props.timeTo : +value
+    const withinRange = this.periodWithinRange(fromValue, toValue)
 
-    onChange({
+    if (!withinRange) {
+      return this.setState({ error: true })
+    }
+
+    this.props.onChange({
       [id]: value,
-      [otherId]: String(otherVal),
+      // [otherId]: String(otherVal),
     })
+
+    return this.setState({ error: null })
   }
 
-  return (
-    <div id='time-period' className='mb4'>
-      <h3 className='mt0 mb2 pb-tiny border-bottom'>Time period</h3>
-      <div className='clearfix'>
-        <div className='col col-5'>
-          <label htmlFor='time-from' className='hide'>Time from</label>
-          <input
-            className='block col-12 field'
-            type='number'
-            id='timeFrom'
-            min={minYear}
-            max={maxYear}
-            onChange={handleChange}
-            value={timeFrom}
-          />
-        </div>
-        <span className='col col-2 center lh-form-field'>to</span>
-        <div className='col col-5'>
-          <label htmlFor='time-to' className='hide'>Time to</label>
-          <input
-            className='block col-12 field'
-            type='number'
-            id='timeTo'
-            min={minYear}
-            max={maxYear}
-            onChange={handleChange}
-            value={timeTo}
-          />
+  periodWithinRange(timeFrom, timeTo) {
+    const { minYear, maxYear, yearRange } = this.state
+
+    if (timeFrom < minYear) return false
+    else if (timeTo > maxYear) return false
+    else if ((timeTo - timeFrom) < yearRange) return false
+    return true
+  }
+
+  render() {
+    const { minYear, maxYear } = this.state
+    return (
+      <div id='time-period' className='mb4'>
+        <h3 className='mt0 mb2 pb-tiny border-bottom'>Time period</h3>
+        {this.state.error && (
+          <p className='red h5'>
+            You must select a period of at least 10 years.
+          </p>
+        )}
+        <div className='clearfix'>
+          <div className='col col-5'>
+            <label htmlFor='time-from' className='hide'>Time from</label>
+            <input
+              className='block col-12 field'
+              type='number'
+              id='timeFrom'
+              min={minYear}
+              max={maxYear}
+              onChange={this.handleChange}
+              value={this.props.timeFrom}
+            />
+          </div>
+          <span className='col col-2 center lh-form-field'>to</span>
+          <div className='col col-5'>
+            <label htmlFor='time-to' className='hide'>Time to</label>
+            <input
+              className='block col-12 field'
+              type='number'
+              id='timeTo'
+              min={minYear}
+              max={maxYear}
+              onChange={this.handleChange}
+              value={this.props.timeTo}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default TimePeriodFilter
