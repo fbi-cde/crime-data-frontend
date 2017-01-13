2732c15995c4ed57ae40e110460ef4e85e01efb1
Brendan Sudol <brendan.sudol@gsa.gov>
2017-01-13 15:23:33 -0500

    type of crime filter updates

--- src/components/CrimeTypeFilter.js
+++ src/components/CrimeTypeFilter.js
@@ -29,11 +29,13 @@ const CrimeTypeFilter = ({ onChange, selected }) => (
   <div id='type-of-crime' className='mb4'>
     <h3 className='mt0 mb2 pb-tiny border-bottom'>Type of crime</h3>
     <FilterGroup
+      title='Violent Crime'
       options={violentCrime}
       onChange={onChange}
       selected={selected}
     />
     <FilterGroup
+      title='Property Crime'
       options={propertyCrime}
       onChange={onChange}
       selected={selected}
--- src/components/FilterGroup.js
+++ src/components/FilterGroup.js
@@ -2,32 +2,37 @@ import React from 'react'
 
 import { slugify } from '../util/text'
 
-const FilterGroup = ({ options, onChange, selected }) => {
+const FilterGroup = ({ options, onChange, selected, title }) => {
   const handleChange = e => onChange({ crime: e.target.value })
 
   return (
-    <div className='mb1 bg-light-blue rounded'>
-      {options.map((o, i) => {
-        const isActive = slugify(o) === slugify(selected)
-        return (
-          <label
-            key={i}
-            className={`py-tiny pl3 block radio ${isActive ? 'bg-darken-2' : ''}`}
-            htmlFor={slugify(o)}
-          >
-            <input
-              checked={isActive}
-              id={slugify(o)}
-              name='crime'
-              onChange={handleChange}
-              type='radio'
-              value={slugify(o)}
-            />
-            <span className='mx1 my-tiny indicator' />
-            {o}
-          </label>
-        )
-      })}
+    <div className='mb2 bg-light-blue rounded overflow-hidden'>
+      {title && <div className='py1 px2 bg-darken-2 bold'>{title}</div>}
+      <div>
+        {options.map((o, i) => {
+          const isActive = slugify(o) === slugify(selected)
+          return (
+            <label
+              key={i}
+              className={
+                `py-tiny px2 block cursor-pointer ${isActive ? 'bg-navy white bold' : ''}`
+              }
+              htmlFor={slugify(o)}
+            >
+              <input
+                className='hide'
+                checked={isActive}
+                id={slugify(o)}
+                name='crime'
+                onChange={handleChange}
+                type='radio'
+                value={slugify(o)}
+              />
+              {o}
+            </label>
+          )
+        })}
+      </div>
     </div>
   )
 }
