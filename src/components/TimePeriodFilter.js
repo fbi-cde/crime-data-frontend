27b5a032118464cef7502cc90b04f6e695085150
Brendan Sudol <brendan.sudol@gsa.gov>
2017-01-15 14:13:02 -0500

    10 year range & error updates

--- src/components/TimePeriodFilter.js
+++ src/components/TimePeriodFilter.js
@@ -1,59 +1,35 @@
 import React from 'react'
 
+const MIN_YEAR = 1960
+const MAX_YEAR = 2015
+const YEAR_WINDOW = 10
+
 class TimePeriodFilter extends React.Component {
   constructor(props) {
     super(props)
     this.handleChange = ::this.handleChange
-    this.state = {
-      error: null,
-      minYear: 1960,
-      maxYear: 2015,
-      yearRange: 10,
-    }
+    this.state = { error: null }
   }
 
   handleChange(e) {
     const { id, value } = e.target
-
     const isFrom = id === 'timeFrom'
-    // const otherId = isFrom ? 'timeTo' : 'timeFrom'
-    // const otherVal = +value + (yearRange * (isFrom ? 1 : -1))
+    const timeFrom = +value - (isFrom ? 0 : YEAR_WINDOW)
+    const timeTo = +value + (isFrom ? YEAR_WINDOW : 0)
+    const outOfRange = timeFrom < MIN_YEAR || timeTo > MAX_YEAR
 
-    const fromValue = (isFrom) ? +value : this.props.timeFrom
-    const toValue = (isFrom) ? this.props.timeTo : +value
-    const withinRange = this.periodWithinRange(fromValue, toValue)
+    this.setState({ error: outOfRange })
+    if (outOfRange) return
 
-    if (!withinRange) {
-      return this.setState({ error: true })
-    }
-
-    this.props.onChange({
-      [id]: value,
-      // [otherId]: String(otherVal),
-    })
-
-    return this.setState({ error: null })
-  }
-
-  periodWithinRange(timeFrom, timeTo) {
-    const { minYear, maxYear, yearRange } = this.state
-
-    if (timeFrom < minYear) return false
-    else if (timeTo > maxYear) return false
-    else if ((timeTo - timeFrom) < yearRange) return false
-    return true
+    this.props.onChange({ timeFrom, timeTo })
   }
 
   render() {
-    const { minYear, maxYear } = this.state
+    const { error } = this.state
+
     return (
       <div id='time-period' className='mb4'>
         <h3 className='mt0 mb2 pb-tiny border-bottom'>Time period</h3>
-        {this.state.error && (
-          <p className='red h5'>
-            You must select a period of at least 10 years.
-          </p>
-        )}
         <div className='clearfix'>
           <div className='col col-5'>
             <label htmlFor='time-from' className='hide'>Time from</label>
@@ -61,8 +37,8 @@ class TimePeriodFilter extends React.Component {
               className='block col-12 field'
               type='number'
               id='timeFrom'
-              min={minYear}
-              max={maxYear}
+              min={MIN_YEAR}
+              max={MAX_YEAR}
               onChange={this.handleChange}
               value={this.props.timeFrom}
             />
@@ -74,13 +50,18 @@ class TimePeriodFilter extends React.Component {
               className='block col-12 field'
               type='number'
               id='timeTo'
-              min={minYear}
-              max={maxYear}
+              min={MIN_YEAR}
+              max={MAX_YEAR}
               onChange={this.handleChange}
               value={this.props.timeTo}
             />
           </div>
         </div>
+        {error && (
+          <p className='mt1 h5 orange'>
+            Please select a 10 year period between {MIN_YEAR} and {MAX_YEAR}.
+          </p>
+        )}
       </div>
     )
   }
