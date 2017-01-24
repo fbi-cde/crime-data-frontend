7688bb7b491813c548c2ed4cdbcc2b763bbb2960
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-23 23:36:11 -0500

    Add filter update actions to <Home />
    Add a lot of event handlers to the home page component so that filters
    get updated and then flowed through the UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,29 @@
 /* eslint no-console: 0 */
 import React from 'react'
 
+import getStateName from '../util/usa'
+import LocationSelect from './LocationSelect'
+import { slugify } from '../util/text'
+import { updateFilters, updateFiltersAndUrl } from '../actions/filterActions'
+
 import MapSvg from '../../img/usa-map.svg'
 
-const Home = () => {
-  const clickMap = e => console.log(e.target.getAttribute('id'))
+const scrollToBottom = () => window.scrollTo(0, window.outerHeight * 1.5)
+
+const Home = ({ appState, dispatch }) => {
+  const { crime, place } = appState.filters
+  const isButtonDisabled = !!(crime && place) || false
+
+  const handleMapClick = e => {
+    const id = e.target.getAttribute('id')
+    dispatch(updateFilters({ place: slugify(getStateName(id)) }))
+  }
+  const handleSearchClick = () => dispatch(updateFiltersAndUrl({}))
+  const selectCrime = e => {
+    const action = updateFilters({ crime: slugify(e.target.value) })
+    dispatch(action)
+  }
+  const selectLocation = e => dispatch(updateFilters(e))
 
   return (
     <div>
@@ -18,10 +37,11 @@ const Home = () => {
             crime statistics.
           </p>
           <img
-            className='absolute right-0'
+            className='absolute right-0 cursor-pointer'
             style={{ bottom: '-30px' }}
             src='/img/arrow-down-circle.svg'
             alt='more information below'
+            onClick={scrollToBottom}
           />
         </div>
       </section>
@@ -30,21 +50,30 @@ const Home = () => {
           <h2 className='mt0 mb4'>Explore by type of crime and location</h2>
           <div className='clearfix mxn2'>
             <div className='sm-col sm-col-5 px2 mb2 sm-m0'>
-              <select className='col-12 bold field'>
-                <option>Location</option>
-              </select>
+              <LocationSelect
+                className='col-12 bold field'
+                onChange={selectLocation}
+                selected={place}
+              />
             </div>
             <div className='sm-col sm-col-5 px2 mb2 sm-m0'>
-              <select className='col-12 bold field'>
+              <select className='col-12 bold field' onChange={selectCrime}>
                 <option>Crime Type</option>
+                <option>Murder</option>
               </select>
             </div>
             <div className='sm-col sm-col-2 px2 mb2 sm-m0'>
-              <button className='col-12 btn btn-primary'>View results</button>
+              <button
+                className='col-12 btn btn-primary'
+                disabled={!isButtonDisabled}
+                onClick={handleSearchClick}
+              >
+                View results
+              </button>
             </div>
           </div>
           <div className='py7 sm-col-9 mx-auto'>
-            <MapSvg onClick={clickMap} />
+            <MapSvg onClick={handleMapClick} />
           </div>
         </div>
       </section>
