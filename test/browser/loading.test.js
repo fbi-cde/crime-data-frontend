9733a0945df1ed9a6576d27c8c767a4aba25013e
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-25 14:36:40 -0500

    need to take a small break

--- test/browser/loading.test.js
+++ test/browser/loading.test.js
@@ -1,26 +1,12 @@
+/* eslint no-undef: 0 */
+
 const assert = require('assert')
-const webdriverio = require('webdriverio')
 
-const options = {
-  desiredCapabilities: {
-    browserName: 'internet explorer',
-    platform: 'Windows 10',
-    version: '11.103',
-    'tunnel-identifier': process.env.CIRCLE_BUILD_NUM,
-  },
-  services: ['sauce'],
-  host: 'ondemand.saucelabs.com',
-  port: 80,
-  user: process.env.SAUCE_USERNAME,
-  key: process.env.SAUCE_ACCESS_KEY,
-  sauceConnect: true,
-}
+console.log(browser.options)
 
-webdriverio
-  .remote(options)
-  .init()
-  .url(`http://127.0.0.1:${process.env.PORT}`)
-  .waitForExist('#app', 2000)
-  .getTitle()
-  .then(title => assert.equal(title, 'crime data explorer'))
-  .end()
+describe('webdriver.io page', () => {
+  it('should have the right title ', () => {
+    browser.url(`https://crime-data-explorer.fr.cloud.gov`)
+    assert.equal(browser.getUrl(), 'WebdriverIO')
+  })
+})
