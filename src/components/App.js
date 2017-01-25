e3e80a491f69ab85ba7184584bb9f31cb9da3efa
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-24 22:25:06 -0500

    Highlight Explorer link when appropriate instead of all the time

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,10 @@ import Footer from './Footer'
 import Glossary from './Glossary'
 import Header from './Header'
 
-const App = ({ appState, children, dispatch }) => (
+const App = ({ appState, children, dispatch, location }) => (
   <div className='site'>
     <Disclaimer />
-    <Header />
+    <Header location={location} />
     <main className='site-main'>
       {children && React.cloneElement(children, { appState, dispatch })}
       <Glossary
