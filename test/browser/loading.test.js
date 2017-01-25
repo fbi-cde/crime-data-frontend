e52f89a268f9b4ea2fb55387132a868959189734
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-01-25 11:17:51 -0500

    Install sc on circle

--- test/browser/loading.test.js
+++ test/browser/loading.test.js
@@ -2,13 +2,23 @@ const webdriverio = require('webdriverio')
 
 const options = {
   desiredCapabilities: {
-    browserName: 'firefox',
+    browserName: 'internet explorer',
+    platform: 'Windows 10',
+    version: '11.103',
   },
-};
+  services: ['sauce'],
+  host: 'ondemand.saucelabs.com',
+  port: 80,
+  user: process.env.SAUCE_USERNAME,
+  key: process.env.SAUCE_KEY,
+  sauceConnect: true,
+}
+
 webdriverio
-    .remote(options)
-    .init()
-    .url('http://www.google.com')
-    .getTitle()
-    .then(title => console.log(`Title was: ${title}`))
-    .end();
+  .remote(options)
+  .init()
+  .url('https://crime-data-explorer.fr.cloud.gov')
+  .waitForExist('#app', 2000)
+  .getTitle()
+  .then(title => console.log(`Title was: ${title}`))
+  .end()
