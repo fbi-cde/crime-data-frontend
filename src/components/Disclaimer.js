a54f9cba15a38c171b434ab3f56ca0e43f48378e
Brendan Sudol <brendansudol@gmail.com>
2017-01-26 17:36:28 -0500

    beta banner iterate (#165)

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -1,24 +1,27 @@
 import React from 'react'
 
 const Disclaimer = () => (
-  <div className='clearfix px2 sm-px6 py1 h6 bg-white line-height-1'>
-    <div className='right xs-hide red'>
-      This site is in beta.&nbsp;
+  <div className='clearfix h6 bg-white line-height-1'>
+    <div className='sm-col p1'>
+      <img
+        className='mr-tiny align-bottom'
+        alt='US flag'
+        width='18'
+        height='12'
+        src='/img/usa-flag.png'
+      />
+      An official website of the United States government
+    </div>
+    <div className='sm-col-right xs-hide'>
+      <span className='mr2 red bold'>This site is in beta.&nbsp;</span>
       <a
-        className='red bold'
+        className='px5 py1 inline-block bold caps white bg-red'
         href='https://github.com/18F/crime-data-explorer/issues'
       >
-        Share your feedback.
+        Feedback
       </a>
     </div>
-    <img
-      className='mr-tiny align-bottom'
-      alt='US flag'
-      width='18'
-      height='12'
-      src='/img/usa-flag.png'
-    />
-    An official website of the United States government
+
   </div>
 )
 
