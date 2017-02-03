983526fb87bd5c6073d2bf253c269a8c1c677112
Brendan Sudol <brendan.sudol@gsa.gov>
2017-02-03 10:42:11 -0500

    add donut chart component

---
+++ src/components/Donut.js
@@ -0,0 +1,68 @@
+import { descending } from 'd3-array'
+import { scaleOrdinal } from 'd3-scale'
+import { arc, pie } from 'd3-shape'
+import React from 'react'
+
+class Donut extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hover: null }
+  }
+
+  rememberValue = id => () => {
+    if (id === this.state.hover) return
+    this.setState({ hover: id })
+  }
+
+  forgetValue = () => {
+    this.setState({ hover: null })
+  }
+
+  render() {
+    const { colors, data, size } = this.props
+    const { hover } = this.state
+
+    const { height, width } = size
+    const r = Math.min(width, height) / 2
+
+    const pieGen = pie().sort(descending)
+    const arcGen = arc().innerRadius(r - 100).outerRadius(r - 20)
+
+    const dataDisplay = pieGen(data)
+
+    const color = scaleOrdinal()
+      .domain(dataDisplay.map((_, i) => i))
+      .range(colors)
+
+    return (
+      <div>
+        <svg
+          className='cursor-pointer'
+          preserveAspectRatio='xMidYMid'
+          viewBox={`0 0 ${size.width} ${size.height}`}
+          style={{ width: '100%', height: '100%' }}
+        >
+          <g transform={`translate(${width / 2}, ${height / 2})`}>
+            {dataDisplay.map((slice, i) => (
+              <path
+                key={i}
+                d={arcGen(slice)}
+                fill={i === hover ? '#000' : color(i)}
+                pointerEvents='all'
+                onMouseOver={this.rememberValue(i)}
+                onMouseOut={this.forgetValue}
+              />
+            ))}
+          </g>
+        </svg>
+      </div>
+    )
+  }
+}
+
+Donut.defaultProps = {
+  size: { width: 640, height: 300 },
+  colors: ['#52687d', '#ff5e50', '#97a7b8'],
+}
+
+export default Donut
