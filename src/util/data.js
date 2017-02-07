aaa5a74e788ce865b493d644a2976c6d8c2bd895
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-02-06 23:16:13 -0500

    Use yaml-loader to import content

--- src/util/data.js
+++ src/util/data.js
@@ -1,207 +1,37 @@
-const timeData = [
-  ['2016-01-01', 5],
-  ['2016-01-02', 7],
-  ['2016-01-03', 3],
-  ['2016-01-04', 5],
-  ['2016-01-05', 10],
-]
+import assaultContent from '../../content/crimes/aggravated-assault.yml'
+import arsonContent from '../../content/crimes/arson.yml'
+import burglaryContent from '../../content/crimes/burglary.yml'
+import cargoTheftContent from '../../content/crimes/cargo-theft.yml'
+import hateCrimeContent from '../../content/crimes/hate-crime.yml'
+import homicideContent from '../../content/crimes/homicide.yml'
+import larcenyContent from '../../content/crimes/larceny-theft.yml'
+import leokaContent from '../../content/crimes/leoka.yml'
+import mvtContent from '../../content/crimes/motor-vehicle-theft.yml'
+import propertyCrimeContent from '../../content/crimes/property-crime.yml'
+import rapeContent from '../../content/crimes/rape.yml'
+import robberyContent from '../../content/crimes/robbery.yml'
+import stateContent from '../../content/locations/states.yml'
+import violentCrimeContent from '../../content/crimes/violent-crime.yml'
 
-const timeData2 = [
-  { date: '2008-01-01', foo: 12, bar: 7 },
-  { date: '2009-01-01', foo: 6, bar: 5 },
-  { date: '2010-01-01', foo: 5, bar: 7 },
-  { date: '2011-01-01', foo: 7, bar: 2 },
-  { date: '2012-01-01', foo: 4, bar: 4 },
-  { date: '2013-01-01', foo: 5, bar: 6 },
-  { date: '2014-01-01', foo: 10, bar: 4 },
-  { date: '2015-01-01', foo: 12, bar: 3 },
-]
+const content = {
+  ...stateContent,
+  crimes: {
+    'aggravated-assault': assaultContent,
+    arson: arsonContent,
+    burglary: burglaryContent,
+    'cargo-theft': cargoTheftContent,
+    'hate-crime': hateCrimeContent,
+    homicide: homicideContent,
+    larceny: larcenyContent,
+    leoka: leokaContent,
+    'motor-vehicle-theft': mvtContent,
+    'property-crime': propertyCrimeContent,
+    rape: rapeContent,
+    robbery: robberyContent,
+    'violent-crime': violentCrimeContent,
+  },
+}
 
-const mapData = { ca: 1, fl: 1 }
-
-const censusData = [
-  {
-    statistic: '11,613,423',
-    label: 'residents',
-  },
-  {
-    statistic: '74%',
-    label: 'covered in UCR program',
-  },
-  {
-    statistic: '831',
-    label: 'law enforcement agencies',
-  },
-  {
-    statistic: '25,992',
-    label: 'officers',
-  },
-  {
-    statistic: '447',
-    label: 'officers per resident',
-  },
-]
-
-const detailRelationshipData = [
-  {
-    key: 'Acquaintance',
-    count: 10083,
-  },
-  {
-    key: 'Unknown',
-    count: 6358,
-  },
-  {
-    key: 'Stranger',
-    count: 3403,
-  },
-  {
-    key: 'Boyfriend',
-    count: 3313,
-  },
-  {
-    key: 'Girlfriend',
-    count: 2794,
-  },
-]
-
-const detailLocationData = [
-  {
-    key: 'Residence/Home2',
-    count: 37355,
-  },
-  {
-    key: 'Highway/Road/Ally',
-    count: 83165,
-  },
-  {
-    key: 'Other/Unknown',
-    count: 65737,
-  },
-  {
-    key: 'Lot/Garage',
-    count: 36629,
-  },
-  {
-    key: 'Department/Discount Store',
-    count: 19493,
-  },
-  {
-    key: 'Service/Gas Station',
-    count: 11546,
-  },
-  {
-    key: 'Grocery/Supermarket',
-    count: 11268,
-  },
-  {
-    key: 'School-Elementary/Secondary',
-    count: 9548,
-  },
-  {
-    key: 'Commercial/Office Building',
-    count: 8039,
-  },
-  {
-    key: 'Restaurant',
-    count: 7055,
-  },
-]
-
-const detailOffenderAge = [
-  {
-    x0: 0,
-    x1: 10,
-    count: 2408,
-  },
-  {
-    x0: 10,
-    x1: 20,
-    count: 76198,
-  },
-  {
-    x0: 20,
-    x1: 30,
-    count: 109999,
-  },
-  {
-    x0: 30,
-    x1: 40,
-    count: 67089,
-  },
-  {
-    x0: 40,
-    x1: 50,
-    count: 44943,
-  },
-  {
-    x0: 50,
-    x1: 60,
-    count: 20510,
-  },
-  {
-    x0: 60,
-    x1: 70,
-    count: 4457,
-  },
-  {
-    x0: 70,
-    x1: 80,
-    count: 1010,
-  },
-  {
-    x0: 80,
-    x1: 90,
-    count: 234,
-  },
-  {
-    x0: 90,
-    x1: 100,
-    count: 29,
-  },
-]
-
-const detailOffenderRace = [
-  {
-    key: 'Asian',
-    count: 836,
-  },
-  {
-    key: 'Black or African American',
-    count: 127140,
-  },
-  {
-    key: 'American Indian or Alaska Native',
-    count: 268,
-  },
-  {
-    key: 'Native Hawaiian or Other Pacific Islander',
-    count: 11,
-  },
-  {
-    key: 'Unknown',
-    count: 52601,
-  },
-  {
-    key: 'White',
-    count: 193505,
-  },
-]
-
-const detailOffenderSex = [
-  {
-    key: 'Female',
-    count: 96501,
-  },
-  {
-    key: 'Male',
-    count: 236783,
-  },
-  {
-    key: 'Unknown',
-    count: 41077,
-  },
-]
 
 /* eslint quote-props: 0 */
 
@@ -261,7 +91,7 @@ const population = {
 
 const violentCrime = [
   'All Violent Crime',
-  'Murder',
+  'Homicide',
   'Rape',
   'Robbery',
   'Aggravated Assault',
@@ -285,15 +115,7 @@ const otherCrime = [
 const crimeTypes = { violentCrime, propertyCrime, otherCrime }
 
 export {
-  censusData,
+  content,
   crimeTypes,
-  detailLocationData,
-  detailOffenderAge,
-  detailOffenderRace,
-  detailOffenderSex,
-  detailRelationshipData,
-  mapData,
   population,
-  timeData,
-  timeData2,
 }
