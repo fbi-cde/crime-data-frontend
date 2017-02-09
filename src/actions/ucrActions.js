bd5abfe76dc1e078b92bcb8a6e455e728e88049b
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-02-09 14:54:43 -0500

    Add UCR related action types, actions and reducer

---
+++ src/actions/ucrActions.js
@@ -0,0 +1,23 @@
+import {
+  UCR_PARTICIPATION_FETCHING,
+  UCR_PARTICIPATION_RECEIVED,
+} from '../actions/actionTypes'
+import api from '../util/api'
+
+export const fetchingUcrParticipation = () => ({
+  type: UCR_PARTICIPATION_FETCHING,
+})
+
+export const receivedUcrParticipation = ({ place, results }) => ({
+  type: UCR_PARTICIPATION_RECEIVED,
+  place,
+  results,
+})
+
+export const fetchUcrParticipation = place => dispatch => {
+  dispatch(fetchingUcrParticipation())
+
+  return api.getUcrParticipation(place).then(d => (
+    dispatch(receivedUcrParticipation(d))
+  ))
+}
---
+++ test/reducers/ucrReducer.test.js
@@ -0,0 +1,59 @@
+/* eslint no-undef: 0 */
+
+import {
+  UCR_PARTICIPATION_FETCHING,
+  UCR_PARTICIPATION_RECEIVED,
+} from '../../src/actions/actionTypes';
+
+import reducer from '../../src/reducers/ucrReducer';
+
+describe('ucrReducer', () => {
+  describe('initial state', () => {
+    it('should have loading set to false', () => {
+      const initialState = reducer(undefined, { type: 'fake' })
+      expect(initialState.loading).toEqual(false)
+    })
+  })
+
+  describe('UCR_PARTICIPATION_FETCHING action type', () => {
+    it('should set loading to true', () => {
+      const action = { type: UCR_PARTICIPATION_FETCHING }
+      const initialState = reducer(undefined, action)
+      expect(initialState.loading).toEqual(true)
+    })
+  })
+
+  describe('UCR_PARTICIPATION_RECEIVED action type', () => {
+    it('should set loading to false', () => {
+      const action = { type: UCR_PARTICIPATION_RECEIVED }
+      const initialState = reducer(undefined, action)
+      expect(initialState.loading).toEqual(false)
+    })
+
+    it('should set place and results from action', () => {
+      const action = {
+        type: UCR_PARTICIPATION_RECEIVED,
+        place: 'fake-place',
+        results: [],
+      }
+      const initialState = reducer(undefined, action)
+      expect(initialState.data[action.place]).toEqual(action.results)
+    })
+
+    it('should not alter previous data in state', () => {
+      const action = {
+        type: UCR_PARTICIPATION_RECEIVED,
+        place: 'fake-other-place',
+        results: ['new-data'],
+      }
+      const initialState = {
+        loading: false,
+        data: {
+          'fake-place': ['existing-data'],
+        },
+      }
+      const state = reducer(initialState, action)
+      expect(state.data['fake-place']).toEqual(['existing-data'])
+    })
+  })
+})
