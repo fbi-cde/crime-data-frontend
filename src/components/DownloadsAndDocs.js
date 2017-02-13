267accd173e858cd4fb9d98f1c9b5a4e48b890b3
Brendan Sudol <brendan.sudol@gsa.gov>
2017-02-13 16:05:13 -0500

    download and docs style love

--- src/components/DownloadsAndDocs.js
+++ src/components/DownloadsAndDocs.js
@@ -8,18 +8,22 @@ const border = 'border-bottom border-blue-lighter'
 const DownloadsAndDocs = () => (
   <section className='bg-white'>
     <div className='px2 py3 container mx-auto'>
-      <h1 className='mt2 sm-mt4 fs-28 sm-fs-40'>Downloads & Documentation</h1>
-      <div className='clearfix mxn1 mb3'>
+      <h1 className='mt4 mb7 pb1 sm-mt4 fs-28 sm-fs-40 border-bottom border-blue-lighter'>
+        Downloads & Documentation
+      </h1>
+      <div className='clearfix mxn1 mb8'>
         <div className='sm-col sm-col-6 px1 mb2'>
-          <div className='p2 sm-p4 bg-blue-lighter'>
-            <h3 className='mt0 fs-22 sans-serif caps'>Crime Data API</h3>
+          <div className='p2 sm-p4 bg-blue-white'>
+            <h3 className='mt0 mb2 pb1 fs-22 sans-serif border-bottom border-red-bright'>
+              Crime Data API
+            </h3>
             <p>
               Bacon ipsum dolor sit amet chuck prosciutto landjaeger ham hock filet mignon
               shoulder hamburger pig venison. Ham bacon corned beef, sausage kielbasa flank
               tongue pig drumstick capicola swine short loin ham hock kevin.
             </p>
             <a
-              className='btn btn-primary bg-blue'
+              className='btn btn-primary btn-sm fs-14'
               href='https://crime-data-api.fr.cloud.gov/swagger-ui/'
             >
               See API documentation
@@ -27,14 +31,21 @@ const DownloadsAndDocs = () => (
           </div>
         </div>
         <div className='sm-col sm-col-6 px1 mb2'>
-          <div className='p2 sm-p4 bg-blue-lighter'>
-            <h3 className='mt0 fs-22 sans-serif caps'>Documentation</h3>
+          <div className='p2 sm-p4 bg-blue-white'>
+            <h3 className='mt0 mb2 pb1 fs-22 sans-serif border-bottom border-red-bright'>
+              Documentation
+            </h3>
             <p>
               Bacon ipsum dolor sit amet chuck prosciutto landjaeger ham hock filet mignon
               shoulder hamburger pig venison. Ham bacon corned beef, sausage kielbasa flank
               tongue pig drumstick capicola swine short loin ham hock kevin.
             </p>
-            <a href='#!' className='btn btn-primary bg-blue'>See UCR documentation</a>
+            <a
+              className='btn btn-primary btn-sm fs-14'
+              href='#!'
+            >
+              See UCR documentation
+            </a>
           </div>
         </div>
       </div>
