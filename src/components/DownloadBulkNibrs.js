d85f6e77c60c350c9ed9e5620318a74c95c4a669
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-02-13 14:27:27 -0500

    Add years 2000 - 2014 for bulk NIBRS download

--- src/components/DownloadBulkNibrs.js
+++ src/components/DownloadBulkNibrs.js
@@ -110,9 +110,21 @@ class DownloadBulkNibrs extends React.Component {
                 id='nibrs-year'
                 onChange={this.handleSelectChange}
               >
+                <option>2000</option>
+                <option>2001</option>
+                <option>2002</option>
+                <option>2003</option>
                 <option>2004</option>
                 <option>2005</option>
                 <option>2006</option>
+                <option>2007</option>
+                <option>2008</option>
+                <option>2009</option>
+                <option>2010</option>
+                <option>2011</option>
+                <option>2012</option>
+                <option>2013</option>
+                <option>2014</option>
               </select>
             </div>
             <button
