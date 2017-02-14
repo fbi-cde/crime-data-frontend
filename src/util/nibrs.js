a42a105a96710f4fc39d2d571f49e4a1a0cd51a6
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2017-02-14 01:35:31 -0500

    Slightly alter location name in NIBRS (#296)
    * Slightly alter location name in NIBRS
    
    To close #192
    
    * Fix code climate errors

--- src/util/nibrs.js
+++ src/util/nibrs.js
@@ -13,6 +13,56 @@ const sexCodes = {
   U: 'Unknown',
 }
 
+const locationNameMapping = {
+  'Abandoned/Condemned Structure': 'Abandoned/Condemned Structure',
+  'Air/Bus/Train Terminal': 'Air/Bus/Train Terminal',
+  'Amusement Park': 'Amusement Park',
+  'Arena/Stadium/Fairgrounds/Coliseum': 'Arena/Stadium/Fairgrounds',
+  'ATM Separate from Bank': 'ATM Separate from Bank',
+  'Auto Dealership New/Used': 'Auto Dealership',
+  'Bank/Savings and Loan': 'Bank',
+  'Bar/Nightclub': 'Bar/Nightclub',
+  'Camp/Campground': 'Campground',
+  'Church Synagogue/Temple': 'Church/Synagogue/Temple/Mosque',
+  'Commercial/Office Building': 'Commercial/Office Building',
+  'Community Center': 'Community Center',
+  'Construction Site': 'Construction Site',
+  'Convenience Store': 'Convenience Store',
+  Cyberspace: 'Cyberspace',
+  'Daycare Facility': 'Daycare Facility',
+  'Department/Discount Store': 'Department/Discount Store',
+  'Dock/Wharf/Freight/Modal Terminal': 'Dock/Wharf/Shipping Terminal',
+  'Drug Store/Dr. s Office/Hospital': 'Drug Store/Doctor’s Office/Hospital',
+  'Farm Facility': 'Farm Facility',
+  'Field/Woods': 'Field/Woods',
+  'Gambling Facility/Casino/Race Track': 'Gambling Facility/Casino/Race Track',
+  'Government/Public Building': 'Government/Public Building',
+  'Grocery/Supermarket': 'Grocery Store',
+  'Highway/Road/Ally': 'Highway/Alley/Street/Sidewalk',
+  'Hotel/Motel/Etc.': 'Hotel/Motel',
+  'Industrial Site': 'Industrial Site',
+  'Jail/Prison': 'Jail/Prison/Corrections Facility',
+  'Lake/Waterway': 'Lake/Waterway/Beach',
+  'Liquor Store': 'Liquor Store',
+  'Military Installation': 'Military Base',
+  'Not Specified': 'Unknown',
+  'Other/Unknown': 'Unknown',
+  'Parking Lot/Garage': 'Parking Garage/Lot',
+  'Park/Playground': 'Park/Playground',
+  'Rental Stor. Facil.': 'Rental Storage Facility',
+  'Residence/Home': 'Residence/Home',
+  'Rest Area': 'Rest Area',
+  Restaurant: 'Restaurant',
+  'School/College': 'School/College',
+  'School-College/University': 'School - College/University',
+  'School-Elementary/Secondary': 'School - Elementary/Secondary',
+  'Service/Gas Station': 'Gas Station',
+  'Shelter-Mission/Homeless': 'Mission/Homeless Shelter',
+  'Shopping Mall': 'Shopping Mall',
+  'Specialty Store': 'Specialty Store',
+  'Tribal Lands': 'Tribal Lands',
+}
+
 // data munging functions
 
 export const reshape = (data, key) => {
@@ -94,14 +144,20 @@ const relationships = data => {
   }
 }
 
+const cleanLocationLabels = l => ({
+  ...l,
+  location_name: locationNameMapping[l.location_name],
+})
+
 const locations = data => {
   const { victimLocationName } = data
+  const locationData = victimLocationName.map(cleanLocationLabels)
 
   return {
     title: 'Location type',
     data: [
       {
-        data: reshape(victimLocationName, 'location_name'),
+        data: reshape(locationData, 'location_name'),
         type: 'table',
       },
     ],
