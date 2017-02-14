ba77a0e3f0f0f33e683545c7ffe34bdd9f07c407
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-02-14 11:10:10 -0500

    Generate NIBRS download list programmatically
    In other parts of the app we know what states submit NIBRS data and
    also what their state codes are. I refactored the view to use this list
    instead of it being all typed out.
    
    Should also satisfy part of
    https://github.com/18F/crime-data-explorer/issues/251#issuecomment-27962
    7428

--- src/components/DownloadBulkNibrs.js
+++ src/components/DownloadBulkNibrs.js
@@ -1,9 +1,27 @@
+import range from 'lodash.range'
 import React from 'react'
+import startCase from 'lodash.startcase'
+
+import ucrProgram from '../../data/ucr-program-participation.json'
+import ucrStateCodes from '../../data/ucr-state-codes.json'
+
+const nibrsStates = Object.keys(ucrProgram).map(s => s).filter(s => {
+  const state = ucrProgram[s]
+  return state.nibrs || state.hybrid
+}).map(s => ({
+  text: startCase(s),
+  value: ucrStateCodes[s],
+}))
+
+const firstYear = 2000
+const numberOfYears = 15
+const nibrsYears = range(numberOfYears).map(x => x + firstYear)
 
 const bulkNibrs = 'http://s3-us-gov-west-1.amazonaws.com/cg-d3f0433b-a53e-4934-8b94-c678aa2cbaf3'
-const createBulkNibrsUrl = (year, state) => (
-  `${bulkNibrs}/${year}/${state}-${year}.zip`
-)
+const createBulkNibrsUrl = (year, state) => {
+  const s = (state < 10) ? `0${state}` : state
+  return `${bulkNibrs}/${year}/${s}-${year}.zip`
+}
 
 const downloadBulkNibrs = (year, state) => {
   const a = document.createElement('a')
@@ -63,44 +81,10 @@ class DownloadBulkNibrs extends React.Component {
                 id='nibrs-state'
                 onChange={this.handleSelectChange}
               >
-                <option value='01'>AL</option>
-                <option value='03'>AR</option>
-                <option value='02'>AZ</option>
-                <option value='05'>CO</option>
-                <option value='06'>CT</option>
-                <option value='08'>DC</option>
-                <option value='07'>DE</option>
-                <option value='14'>IA</option>
-                <option value='11'>ID</option>
-                <option value='12'>IL</option>
-                <option value='13'>IN</option>
-                <option value='15'>KS</option>
-                <option value='16'>KY</option>
-                <option value='17'>LA</option>
-                <option value='20'>MA</option>
-                <option value='18'>ME</option>
-                <option value='21'>MI</option>
-                <option value='24'>MO</option>
-                <option value='23'>MS</option>
-                <option value='25'>MT</option>
-                <option value='26'>NE</option>
-                <option value='33'>ND</option>
-                <option value='28'>NH</option>
-                <option value='34'>OH</option>
-                <option value='35'>OK</option>
-                <option value='36'>OR</option>
-                <option value='37'>PA</option>
-                <option value='38'>RI</option>
-                <option value='39'>SC</option>
-                <option value='40'>SD</option>
-                <option value='41'>TN</option>
-                <option value='42'>TX</option>
-                <option value='43'>UT</option>
-                <option value='45'>VA</option>
-                <option value='44'>VT</option>
-                <option value='46'>WA</option>
-                <option value='48'>WI</option>
-                <option value='47'>WV</option>
+                <option disabled selected>Select a place</option>
+                {nibrsStates.map((s, i) => (
+                  <option key={i} value={s.value}>{s.text}</option>
+                ))}
               </select>
             </div>
             <div className='sm-col sm-col-4 px1 mb2 sm-m0'>
@@ -110,21 +94,10 @@ class DownloadBulkNibrs extends React.Component {
                 id='nibrs-year'
                 onChange={this.handleSelectChange}
               >
-                <option>2000</option>
-                <option>2001</option>
-                <option>2002</option>
-                <option>2003</option>
-                <option>2004</option>
-                <option>2005</option>
-                <option>2006</option>
-                <option>2007</option>
-                <option>2008</option>
-                <option>2009</option>
-                <option>2010</option>
-                <option>2011</option>
-                <option>2012</option>
-                <option>2013</option>
-                <option>2014</option>
+                <option disabled selected>Select a year</option>
+                {nibrsYears.map((y, i) => (
+                  <option key={i}>{y}</option>
+                ))}
               </select>
             </div>
             <div className='sm-col sm-col-3 md-col-2 px1 mb2 sm-m0'>
