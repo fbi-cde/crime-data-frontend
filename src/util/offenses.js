7db49dfa7c1ff37940eaf79e6a06cebcebc4bce4
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2017-02-21 13:02:25 -0500

    Use explorer_offense for consistent mapping (#361)
    The API mapping can be found here:
    https://github.com/18F/crime-data-api/blob/master/crime_data/common/base
    .py#L507

--- src/util/offenses.js
+++ src/util/offenses.js
@@ -1,29 +1,31 @@
+import { slugify } from './text'
+
 const offenses = {
-  'aggravated-assault': 'Assault',
-  burglary: 'Burglary',
-  larceny: 'Larceny',
-  'motor-vehicle-theft': 'Moter vehicle theft',
-  homicide: 'Homicide',
+  'aggravated-assault': 'aggravated-assault',
+  burglary: 'burglary',
+  larceny: 'larceny',
+  'motor-vehicle-theft': 'motor-vehicle-theft',
+  homicide: 'homicide',
   'property-crime': 'Property',
-  rape: 'Rape',
-  robbery: 'Robbery',
+  rape: 'rape',
+  robbery: 'robbery',
   'violent-crime': 'Violent',
 }
 const ids = Object.keys(offenses)
 
 const offenseParams = {
-  'aggravated-assault': 'offense',
-  burglary: 'offense',
-  larceny: 'offense',
-  'motor-vehicle-theft': 'offense',
+  'aggravated-assault': 'explorer_offense',
+  burglary: 'explorer_offense',
+  larceny: 'explorer_offense',
+  'motor-vehicle-theft': 'explorer_offense',
   homicide: 'offense_category',
   'property-crime': 'classification',
-  rape: 'offense',
-  robbery: 'offense',
+  rape: 'explorer_offense',
+  robbery: 'explorer_offense',
   'violent-crime': 'classification',
 }
 
-const mapToApiOffense = o => offenses[o]
+const mapToApiOffense = o => offenses[o] || slugify(o)
 const mapToApiOffenseParam = o => offenseParams[o]
 
 export default ids
