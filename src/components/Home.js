6e5c7e7a49d84a33ad9d64527142e59ced2a1016
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-02-21 19:23:16 -0500

    Use slice instead of checking index to display 3 other datasets
    https://github.com/18F/crime-data-explorer/pull/363#discussion_r10234892
    4

--- src/components/Home.js
+++ src/components/Home.js
@@ -114,22 +114,19 @@ const Home = ({ appState, dispatch, location }) => {
           </div>
           <h2 className='mt0 mb3 fs-22 sm-fs-32'>Data downloads</h2>
           <div className='flex flex-wrap mxn2'>
-            {otherDataSets.map((d, i) => {
-              const show = (i < 3)
-              return show && (
-                <div key={i} className='flex sm-col sm-col-6 md-col-4 px2 mb2 sm-mb4'>
-                  <div className='px3 py2 bg-blue-white'>
-                    <div className='mb1 pb1 fs-ch2 bold border-bottom border-red-bright'>
-                      {d.title}
-                    </div>
-                    <p className='mb2'>{d.description}</p>
-                    <button className='mb1 btn btn-primary btn-sm fs-14'>
-                      View details
-                    </button>
+            {otherDataSets.slice(0, 3).map((d, i) => (
+              <div key={i} className='flex sm-col sm-col-6 md-col-4 px2 mb2 sm-mb4'>
+                <div className='px3 py2 bg-blue-white'>
+                  <div className='mb1 pb1 fs-ch2 bold border-bottom border-red-bright'>
+                    {d.title}
                   </div>
+                  <p className='mb2'>{d.description}</p>
+                  <button className='mb1 btn btn-primary btn-sm fs-14'>
+                    View details
+                  </button>
                 </div>
-              )
-            })}
+              </div>
+            ))}
           </div>
         </div>
       </section>
