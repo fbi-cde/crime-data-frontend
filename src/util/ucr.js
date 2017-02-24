f0587642a2dbe5f7f64e136f9e3ca217fd1aef84
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-02-24 17:21:58 -0500

    UCR utility should slugify inputs

--- src/util/ucr.js
+++ src/util/ucr.js
@@ -1,5 +1,6 @@
 import data from '../../data/ucr-program-participation.json'
+import { slugify } from './text'
 
-const ucrParticipation = state => data[state] || null
+const ucrParticipation = state => data[slugify(state)] || null
 
 export default ucrParticipation
