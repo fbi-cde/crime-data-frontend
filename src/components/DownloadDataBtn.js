52ce5f37bd01a0dd2da6a006bde791f606f72012
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-02-26 17:00:38 -0500

    Refactor CSV shaping functions into utility

--- src/components/DownloadDataBtn.js
+++ src/components/DownloadDataBtn.js
@@ -1,29 +1,10 @@
-import flatten from 'lodash.flatten'
 import React from 'react'
 
-const getCols = data => {
-  const firstRow = data[0]
-  const colsToFlatten = Object.keys(firstRow).map(key => {
-    if (typeof firstRow[key] !== 'object') return key
-    return Object.keys(firstRow[key]).map(childKey => `${key}.${childKey}`)
-  })
-
-  return flatten(colsToFlatten)
-}
-
-const shapeDownloadData = data => {
-  const cols = getCols(data)
-  const values = data.map(d => cols.map(c => {
-    const split = c.split('.')
-    return (split.length > 1) ? d[split[0]][split[1]] : d[split]
-  }))
-
-  return `${cols.join(',')}\n${values.join('\n')}`
-}
+import jsonToCsv from '../util/csv'
 
 const downloadData = (fname, data) => {
   const file = `${fname}.csv`
-  const dataStr = shapeDownloadData(data)
+  const dataStr = jsonToCsv(data)
 
   if (window.navigator.msSaveBlob) {
     const blob = new Blob([dataStr], { type: 'text/csv' })
