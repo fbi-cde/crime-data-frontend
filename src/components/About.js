eb57809664b83b65d18c862481a37c059418dacc
RyanSibley <ryannicole08@gmail.com>
2017-02-27 16:27:20 -0500

    made a few more changes and minor edits
    I’m worried one of my deletions will affect the design/layout. If so
    maybe I can change it back and find some copy to make things easier.

--- src/components/About.js
+++ src/components/About.js
@@ -50,7 +50,7 @@ const About = () => (
         </h1>
         <div className='mb7 md-col-10 fs-18 sm-fs-24 serif'>
           <p>
-            The Crime Data Explorer is a tool built to publish nation-wide crime data collected by the FBI in a digital format. The tool allows you to view trends and download bulk data allowing you to get a better understanding of crime across the country.
+            The Crime Data Explorer publishes nation-wide crime data collected by the FBI in a digital format. The tool allows you to view trends and download bulk data allowing you to get a better understanding of crime across the country.
           </p>
         </div>
         <h2 className='mt0 mb4 pb1 fs-22 sm-fs-32 border-bottom border-blue-light'>
@@ -117,19 +117,16 @@ const About = () => (
       <div className='py7 container mx-auto'>
         <div className='clearfix mxn2'>
           <div className='sm-col sm-col-4 px2 mb4 sm-mb0'>
-            <h3 className='mt0 mb2 fs-22 sans-serif'>1. About the Uniform Crime Reporting (UCR) Program</h3>
+            <h3 className='mt0 mb2 fs-22 sans-serif'>About the Uniform Crime Reporting (UCR) Program</h3>
             <p className='mb2 sm-mb4'>
           <ul>
-            <li>a href="https://ucr.fbi.gov/">UCR Home</a></li>
+            <li><a href="https://ucr.fbi.gov/">UCR Home</a></li>
             <li><a href="https://ucr.fbi.gov/new-ucr-project">New UCR</a></li>
          </ul>
              </p>
-            <button className='btn btn-primary btn-sm fs-14'>
-              View details
-            </button>
           </div>
           <div className='sm-col sm-col-4 px2 mb4 sm-mb0'>
-            <h3 className='mt0 mb2 fs-22 sans-serif'>2. UCR documentation</h3>
+            <h3 className='mt0 mb2 fs-22 sans-serif'>UCR documentation</h3>
             <p className='mb2 sm-mb4'>
             <ul>
               <li>a href="https://ucr.fbi.gov/ucr-publications">Publications and reports</a></li>
@@ -139,29 +136,11 @@ const About = () => (
               <li><a href="https://ucr.fbi.gov/ucr-program-data-collections#Summary">LEOKA documentation</a></li>
            </ul>
             </p>
-            <button className='btn btn-primary btn-sm fs-14'>
-              View details
-            </button>
-          </div>
-          <div className='sm-col sm-col-4 px2 mb4 sm-mb0'>
-            <h3 className='mt0 mb2 fs-22 sans-serif'>3. New UCR</h3>
-            <p className='mb2 sm-mb4'>
-              The New UCR will improve the accuracy and timeliness of
-              the crime data collection and delivery process. The national
-              UCR Program plans to have the new system fully operational
-              in 2017.
-            </p>
-            <button className='btn btn-primary btn-sm fs-14'>
-              View details
-            </button>
-          </div>
-        </div>
-      </div>
     </section>
     <section className='px2 bg-blue white'>
       <div className='py7 container mx-auto'>
         <h2 className='mt0 mb4 pb1 fs-22 sm-fs-32 border-bottom border-red-bright'>
-          Submit your feedback
+          More to come
         </h2>
         <p>
           This project is part of an ongoing effort to improve and promote transparency behind the nation’s crime statistics. We’re working on adding more datasets to this tool, and we’re continuing to develop new features based on your feedback.
