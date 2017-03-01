2e53c732141e6e84393a21cd49f61ed7f3a9ea32
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-03-01 18:38:52 -0500

    Add tests for glossary actions

--- test/actions/glossary.test.js
+++ test/actions/glossary.test.js
@@ -3,9 +3,10 @@
 import {
   GLOSSARY_HIDE,
   GLOSSARY_SHOW,
+  GLOSSARY_SHOW_TERM,
 } from '../../src/actions/constants'
 
-import { hideGlossary, showGlossary } from '../../src/actions/glossary'
+import { hideGlossary, showGlossary, showTerm } from '../../src/actions/glossary'
 
 describe('glossary', () => {
   describe('hideGlossary()', () => {
@@ -19,6 +20,13 @@ describe('glossary', () => {
     it('should return a GLOSSARY_SHOW action', () => {
       const actual = showGlossary()
       expect(actual.type).toEqual(GLOSSARY_SHOW)
+    })
+  })
+
+  describe('showTerm()', () => {
+    it('should return a GLOSSARY_SHOW_TERM action', () => {
+      const actual = showTerm('fake-term')
+      expect(actual.type).toEqual(GLOSSARY_SHOW_TERM)
     })
   })
 })
