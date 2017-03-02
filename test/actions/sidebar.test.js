7ec94765fd5b2585ddcd5c348ecc8855a049a301
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-03-02 11:31:53 -0500

    Add sidebar actions tests

---
+++ test/actions/sidebar.test.js
@@ -0,0 +1,20 @@
+/* eslint no-undef: 0 */
+
+import { SIDEBAR_HIDE, SIDEBAR_SHOW } from '../../src/actions/constants'
+import { hideSidebar, showSidebar } from '../../src/actions/sidebar'
+
+describe('sidebar actions', () => {
+  describe('hideSidebar()', () => {
+    it('should return a SIDEBAR_HIDE action', () => {
+      const actual = hideSidebar()
+      expect(actual.type).toEqual(SIDEBAR_HIDE)
+    })
+  })
+
+  describe('showSidebar()', () => {
+    it('should return a SIDEBAR_SHOW action', () => {
+      const actual = showSidebar()
+      expect(actual.type).toEqual(SIDEBAR_SHOW)
+    })
+  })
+})
