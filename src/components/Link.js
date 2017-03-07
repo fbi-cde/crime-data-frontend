aaa716c5e51639ee94589e931a52895cb2af5466
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-03-07 11:12:28 -0800

    Add <Link /> component to handle internal and external links
    Fixes #516

---
+++ src/components/Link.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link as RouterLink } from 'react-router'
+
+const Link = props => {
+  const { to } = props
+  let el = (<RouterLink {...props}>{props.children}</RouterLink>)
+
+  if (to.includes('http://') || to.includes('https://')) {
+    el = (<a href={to} {...props}>{props.children}</a>)
+  }
+
+  return el
+}
+
+export default Link
