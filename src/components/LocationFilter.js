23fa4043b42434fea512479f179d628f2320d02e
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-03-09 11:58:01 -0800

    Remove place thumbnail from sidebar for now

--- src/components/LocationFilter.js
+++ src/components/LocationFilter.js
@@ -2,12 +2,10 @@ import React from 'react'
 import startCase from 'lodash.startcase'
 
 import LocationSelect from './LocationSelect'
-import PlaceThumbnail from './PlaceThumbnail'
 
 const LocationFilter = ({ onChange, selected }) => (
   <div id='location' className='mb4'>
     <div className='mb2 fs-22 bold border-bottom'>Location</div>
-    <PlaceThumbnail place={selected} />
     <LocationSelect
       onChange={onChange}
       selected={startCase(selected)}
