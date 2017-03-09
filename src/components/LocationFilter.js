4e614d2b1d678c849332370eb1ef937fe697bd79
Brendan Sudol <brendan.sudol@gsa.gov>
2017-03-09 15:26:29 -0500

    tweak spacing

--- src/components/LocationFilter.js
+++ src/components/LocationFilter.js
@@ -5,7 +5,7 @@ import LocationSelect from './LocationSelect'
 
 const LocationFilter = ({ onChange, selected }) => (
   <div id='location' className='mb4'>
-    <div className='mb2 fs-22 bold border-bottom'>Location</div>
+    <div className='mb3 fs-22 bold border-bottom'>Location</div>
     <LocationSelect
       onChange={onChange}
       selected={startCase(selected)}
