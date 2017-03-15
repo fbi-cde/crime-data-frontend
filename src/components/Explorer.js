8ce5cd07c05571d905a74115134f66fe63897fc4
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-03-14 21:52:51 -0400

    Use sticky classes for sidebar toggle
    Uses classes enables us to use postcss to generate the appropriate
    prefixes

--- src/components/Explorer.js
+++ src/components/Explorer.js
@@ -112,7 +112,7 @@ class Explorer extends React.Component {
 
     return (
       <div className='site-wrapper'>
-        <div style={{ position: 'sticky', top: 0 }}>
+        <div className='sticky top-0'>
           <div className='inline-block p1 bg-red-bright md-hide lg-hide'>
             <button
               className='btn p1 line-height-1 border-none'
