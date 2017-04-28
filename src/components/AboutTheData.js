2b2c331858d540b7ff0508cf3e6fc71b5e9cb9f4
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2017-04-28 16:53:25 -0400

    Some changes to <AboutTheData /> (#720)
    * Trigger glossary terms from <AboutTheData />
    
    If a clicked link in the <AboutTheData /> component is to
    `#glossary?term=:termId` then trigger the glossary with that term. Add
    some examples of this behavior to the violent crime content.
    
    Underline all links in the component's main text section.
    
    Refs #467
    
    * Show further reading box in <AboutTheData /> on small screens
    
    Refs #715
    
    * Apply css to "About the Data" links in md render

--- src/components/AboutTheData.js
+++ src/components/AboutTheData.js
@@ -1,52 +1,93 @@
+import lowerCase from 'lodash.lowercase'
 import md from 'markdown-it'
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import content from '../util/content'
 
 const markdown = md()
+const defaultLinkRender =
+  markdown.renderer.rules.link_open ||
+  ((tokens, idx, options, env, self) => self.renderToken(tokens, idx, options))
 
-const AboutTheData = ({ crime }) => {
-  const { caveats, links } = content.crimes[crime]
+markdown.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+  tokens[idx].attrPush(['class', 'underline'])
+  return defaultLinkRender(tokens, idx, options, env, self)
+}
 
-  return (
-    <div>
-      <h3 className="mt0 mb4 fs-22 sm-fs-26">About the data</h3>
-      <div className="lg-flex">
-        <div className="flex-auto mb1 fs-14 sm-fs-16 black">
-          <p>
-            The FBI collects crime data through the&nbsp;
-            <a className="blue underline" href="https://ucr.fbi.gov/">
-              Uniform Crime Reporting (UCR) Program
-            </a>.
-          </p>
-          {caveats.map((c, i) => (
-            <div key={i}>
-              <div className="bold">{c.heading}</div>
-              {/* eslint react/no-danger: 0 */}
-              <div
-                dangerouslySetInnerHTML={{ __html: markdown.render(c.text) }}
-              />
-            </div>
-          ))}
-        </div>
-        <div
-          className="flex-none ml3 xs-hide sm-hide md-hide"
-          style={{ width: 300 }}
-        >
-          <div className="p2 sm-px4 sm-py3 bg-blue white">
-            <h4 className="mt0 mb1 fs-18">Further reading</h4>
-            <ul className="m0 p0 fs-14 sm-fs-16 left-bars">
-              {links.map((l, i) => (
-                <li key={i} className="mb1">
-                  <a className="white" href={l.url}>{l.text}</a>
-                </li>
+// adopted from examples in markdown-it docs
+// https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
+
+class AboutTheData extends React.Component {
+  componentDidMount() {
+    document.addEventListener('click', this.triggerGlossaryTerm)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.triggerGlossaryTerm)
+  }
+
+  triggerGlossaryTerm = event => {
+    const { onTermClick } = this.props
+    const { target } = event
+    if (!target.closest('.about-the-data .caveats')) return
+    if (!target.href || !target.href.match(/#glossary\?term=/)) return
+    const term = lowerCase(target.href.split('term=')[1])
+
+    event.preventDefault()
+    onTermClick(term)
+  }
+
+  render() {
+    const { crime } = this.props
+    const { caveats, links } = content.crimes[crime]
+
+    return (
+      <div className="about-the-data">
+        <h3 className="mt0 mb4 fs-22 sm-fs-26">About the data</h3>
+        <div className="lg-flex">
+          <div className="flex-auto mb1 fs-14 sm-fs-16 black">
+            <p>
+              The FBI collects crime data through the&nbsp;
+              <a className="blue underline" href="https://ucr.fbi.gov/">
+                Uniform Crime Reporting (UCR) Program
+              </a>.
+            </p>
+            <div className="caveats">
+              {caveats.map((c, i) => (
+                <div key={i}>
+                  <div className="bold">{c.heading}</div>
+                  {/* eslint react/no-danger: 0 */}
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: markdown.render(c.text),
+                    }}
+                  />
+                </div>
               ))}
-            </ul>
+            </div>
+          </div>
+          <div className="flex-none mb2 col-12 lg-block lg-col-4 lg-ml3 lg-mb0">
+            <div className="p2 sm-px4 sm-py3 bg-blue white">
+              <h4 className="mt0 mb1 fs-18">Further reading</h4>
+              <ul className="m0 p0 fs-14 sm-fs-16 left-bars">
+                {links.map((l, i) => (
+                  <li key={i} className="mb1">
+                    <a className="white" href={l.url}>{l.text}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+}
+
+AboutTheData.propTypes = {
+  crime: PropTypes.string.isRequired,
+  onTermClick: PropTypes.func.isRequired,
 }
 
 export default AboutTheData
