b43677023f0e7efd36cb5773779f116cd7093526
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2017-05-08 10:27:04 -0700

    Sort summary data by date (#738)
    * Sort summary data by date
    
    * Sort summary data by year key without converting to date

--- src/util/summary.js
+++ src/util/summary.js
@@ -7,6 +7,7 @@ const mungeSummaryData = ({ crime, summaries, place, since, until }) => {
       if (!since || !until) return true
       return d.year >= since && d.year <= until
     })
+    .sort((a, b) => a.year - b.year)
     .map(year => {
       const data = { date: year.year }
       keys.forEach(key => {
