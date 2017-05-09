198a6858a8a4426a0f65fb4b52062c56f3ad80f1
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-05-08 23:14:22 -0700

    Source trend charts with FBI estimates

--- src/components/TrendSourceText.js
+++ src/components/TrendSourceText.js
@@ -3,52 +3,20 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 import Term from './Term'
-import ucrParticipation from '../util/ucr'
 
 const TrendSourceText = ({ dispatch, place, since, until }) => {
-  const ucr = ucrParticipation(place)
-
-  let text
-  if (ucr.srs && ucr.nibrs) {
-    text = (
-      <span>
-        <Term id="summary reporting system (srs)" dispatch={dispatch}>
-          Summary (SRS)
-        </Term>
-        {' and '}
-        <Term
-          id="national incident-based reporting system (nibrs)"
-          dispatch={dispatch}
-        >
-          summarized incident (NIBRS)
-        </Term>
-      </span>
-    )
-  } else if (ucr.srs && !ucr.nibrs) {
-    text = (
-      <span>
-        <Term id="summary reporting system (srs)" dispatch={dispatch}>
-          Summary (SRS)
-        </Term>
-      </span>
-    )
-  } else if (!ucr.srs && ucr.nibrs) {
-    text = (
-      <span>
-        <Term
-          id="national incident-based reporting system (nibrs)"
-          dispatch={dispatch}
-        >
-          Summarized incident (NIBRS)
-        </Term>
-      </span>
-    )
-  }
+  const estimated = (
+    <span>
+      <Term id="estimated data" dispatch={dispatch}>
+        estimated
+      </Term>
+    </span>
+  )
 
   return (
     <div className="center italic fs-12 mb8">
       <p>
-        Source: {text} data from {startCase(place)}, {since}–{until}.
+        Source: FBI, {estimated} data for {startCase(place)}, {since}–{until}.
       </p>
     </div>
   )
