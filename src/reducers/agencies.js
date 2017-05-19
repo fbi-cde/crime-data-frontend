1eeebe78dc42f9b7660082adb16f1f2197d7a6da
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-05-19 15:48:48 -0400

    Hackily load in agencies by state data
    While we wait for more specific endpoints that won't require us to
    crawl hundreds of pages, I loaded all the agencies by state into the
    reducer. These are minimal agency objects with the ORI as the key and
    the name of the agencies

--- src/reducers/agencies.js
+++ src/reducers/agencies.js
@@ -1,7 +1,26 @@
+import initialData from '../../data/agencies-by-state.json'
+import { slugify } from '../util/text'
+import lookupUsa from '../util/usa'
+
 import { AGENCY_FETCHING, AGENCY_RECEIVED } from '../actions/constants'
 
+// hack to load in all the data while we wait for more specific endoints
+const agencies = Object.keys(initialData)
+  .filter(usState => lookupUsa(usState))
+  .map(usState => ({
+    key: slugify(lookupUsa(usState)),
+    value: initialData[usState],
+  }))
+  .reduce(
+    (accu, next) => ({
+      ...accu,
+      [next.key]: next.value,
+    }),
+    {},
+  )
 const initialState = {
   loading: false,
+  ...agencies,
 }
 
 export default (state = initialState, action) => {
