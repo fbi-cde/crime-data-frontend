a0d5c52e99b6089c0dc3ce9cac885dd2d06818a6
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-05-22 16:43:51 -0400

    Add getAgency helper to ori util

--- src/util/ori.js
+++ src/util/ori.js
@@ -2,3 +2,8 @@ import { slugify } from './text'
 import lookupUsa from './usa'
 
 export const oriToState = ori => slugify(lookupUsa(ori.slice(0, 2)))
+
+export const getAgency = (agencies, ori) => {
+  const usState = oriToState(ori)
+  return agencies.data[usState][ori]
+}
