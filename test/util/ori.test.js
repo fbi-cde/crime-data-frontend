8689b78518068bcc0bfcd90efd68786a83346085
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-06-01 18:55:42 -0400

    Handle Nebraska ORIs
    Closes https://github.com/18F/crime-data-frontend/issues/806

--- test/util/ori.test.js
+++ test/util/ori.test.js
@@ -4,7 +4,12 @@ import { oriToState } from '../../src/util/ori'
 
 describe('ori utility', () => {
   it('should return the state an ORI is in', () => {
-    const actual = oriToState('NJ123456')
-    expect(actual).toEqual('new-jersey')
+    expect(oriToState('FL123456')).toEqual('florida')
+    expect(oriToState('NB123456')).toEqual('nebraska')
+    expect(oriToState('NJ123456')).toEqual('new-jersey')
+    expect(oriToState('NY123456')).toEqual('new-york')
+
+    // nebraska changed from NB to NE
+    // http://about.usps.com/who-we-are/postal-history/state-abbreviations.pdf
   })
 })
