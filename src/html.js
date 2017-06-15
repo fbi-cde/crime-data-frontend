a9add8bc71c7a87799fd53b98853570248d9aa24
Brendan Sudol <brendan.sudol@gsa.gov>
2017-06-15 09:24:24 -0400

    add favicon

--- src/html.js
+++ src/html.js
@@ -8,6 +8,7 @@ export default (content, state) =>
     <head>
       <meta charset='utf-8'>
       <meta name='viewport' content='width=device-width, initial-scale=1'>
+      <link rel='icon' type='image/png' sizes='32x32' href='/img/favicon.png'>
       ${sharingMetaTags(state)}
       <title>Crime Data Explorer</title>
       <link href='/app.css' rel='stylesheet'>
