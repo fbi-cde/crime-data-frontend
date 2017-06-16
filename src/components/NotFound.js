e0f2c94bb395fc9ad87cd063da6882792ec5e1e3
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2017-06-16 10:45:43 -0400

    Increase footer icon size by 30% (#954)
    * Increase footer logo size by 30%
    
    Refs #951
    
    * Make 404 more legible on small displays

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -8,7 +8,7 @@ import { showFeedback } from '../actions/feedback'
 
 const NotFound = ({ dispatch }) => (
   <div className="container mb8 mx-auto px2 pt8">
-    <h1 className="mt0 fs-40 col-7">
+    <h1 className="mt0 fs-40 col-12 sm-col-7">
       We couldn{"'"}t find the page you are looking for.
     </h1>
     <p className="m0 mb4 fs-24 serif">Please check the link and try again.</p>
