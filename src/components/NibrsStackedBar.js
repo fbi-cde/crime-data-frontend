7677b9abffa1c46d7cf45e9ab6cfeea434c0066c
Brendan Sudol <brendan.sudol@gsa.gov>
2017-06-16 11:50:12 -0400

    remove bold, simplify stacked bar

--- src/components/NibrsStackedBar.js
+++ src/components/NibrsStackedBar.js
@@ -6,97 +6,67 @@ import React from 'react'
 
 import NibrsStackedBarDetails from './NibrsStackedBarDetails'
 
-class NibrsStackedBar extends React.Component {
-  state = { hover: null }
+const NibrsStackedBar = ({ colors, keys, data, margin, size, title }) => {
+  const height = size.height - margin.top - margin.bottom
+  const width = size.width - margin.left - margin.right
+  const totalCt = data.reduce((a, b) => a + +b.count, 0)
+  const x = scaleBand().domain([null]).rangeRound([0, width]).padding(0.4)
+  const y = scaleLinear().domain([0, totalCt]).rangeRound([height, 0]).nice()
 
-  rememberValue = d => () => {
-    this.setState({ hover: d })
-  }
+  const colorMap = scaleOrdinal()
+    .domain(keys || data.map(d => d.key).sort())
+    .range(colors)
 
-  forgetValue = () => {
-    this.setState({ hover: null })
-  }
+  const lookup = Object.assign(...data.map(d => ({ [d.key]: +d.count })))
+  const dataClean = keys
+    ? Object.assign(...keys.map(k => ({ [k]: lookup[k] || 0 })))
+    : { ...lookup }
 
-  render() {
-    const { colors, keys, data, margin, size, title } = this.props
-    const { hover } = this.state
+  const stackGen = stack().keys(Object.keys(dataClean)).order(stackOrderReverse)
 
-    const height = size.height - margin.top - margin.bottom
-    const width = size.width - margin.left - margin.right
+  const dataStacked = stackGen([dataClean])
+  const dataEntries = entries(dataClean)
 
-    const totalCt = data.reduce((a, b) => a + +b.count, 0)
-
-    const x = scaleBand().domain([null]).rangeRound([0, width]).padding(0.4)
-
-    const y = scaleLinear().domain([0, totalCt]).rangeRound([height, 0]).nice()
-
-    const colorMap = scaleOrdinal()
-      .domain(keys || data.map(d => d.key).sort())
-      .range(colors)
-
-    const lookup = Object.assign(...data.map(d => ({ [d.key]: +d.count })))
-    const dataClean = keys
-      ? Object.assign(...keys.map(k => ({ [k]: lookup[k] || 0 })))
-      : { ...lookup }
-
-    const stackGen = stack()
-      .keys(Object.keys(dataClean))
-      .order(stackOrderReverse)
-
-    const dataStacked = stackGen([dataClean])
-    const dataEntries = entries(dataClean)
-
-    return (
-      <div className="mb2 pb2 border-bottom border-blue-light">
-        <div className="mb2 blue bold">{title}</div>
-        <div className="flex flex-wrap items-end mxn1 mb2">
-          <div className="col px1" style={{ width: '55%' }}>
-            <svg
-              className="block"
-              preserveAspectRatio="xMidYMid"
-              viewBox={`0 0 ${size.width} ${size.height}`}
-              style={{ width: '100%', height: '100%' }}
-            >
-              <g transform={`translate(${margin.left}, ${margin.top})`}>
-                {dataStacked.map(d =>
-                  <g
-                    key={d.key}
-                    className="cursor-pointer"
-                    fill={colorMap(d.key)}
-                    pointerEvents="all"
-                    onMouseOver={this.rememberValue(d.key)}
-                    onMouseOut={this.forgetValue}
-                  >
-                    <rect
-                      x={x(null)}
-                      y={y(d[0][1])}
-                      height={y(d[0][0]) - y(d[0][1])}
-                      width={x.bandwidth()}
-                    />
-                  </g>,
-                )}
-                <g className="axis" transform={`translate(0, ${height})`}>
-                  <line x2={width} strokeWidth="1" />
-                </g>
+  return (
+    <div className="mb2 pb2 border-bottom border-blue-light">
+      <div className="mb2 blue bold">{title}</div>
+      <div className="flex flex-wrap items-end mxn1 mb2">
+        <div className="col px1" style={{ width: '55%' }}>
+          <svg
+            className="block"
+            preserveAspectRatio="xMidYMid"
+            viewBox={`0 0 ${size.width} ${size.height}`}
+            style={{ width: '100%', height: '100%' }}
+          >
+            <g transform={`translate(${margin.left}, ${margin.top})`}>
+              {dataStacked.map(d =>
+                <g key={d.key} fill={colorMap(d.key)}>
+                  <rect
+                    x={x(null)}
+                    y={y(d[0][1])}
+                    height={y(d[0][0]) - y(d[0][1])}
+                    width={x.bandwidth()}
+                  />
+                </g>,
+              )}
+              <g className="axis" transform={`translate(0, ${height})`}>
+                <line x2={width} strokeWidth="1" />
               </g>
-            </svg>
-          </div>
-
-          <div className="col px1" style={{ width: '45%' }}>
-            <NibrsStackedBarDetails
-              colorMap={colorMap}
-              data={dataEntries}
-              onMouseOver={this.rememberValue}
-              onMouseOut={this.forgetValue}
-              selected={hover}
-              total={totalCt}
-            />
-          </div>
-
+            </g>
+          </svg>
         </div>
+
+        <div className="col px1" style={{ width: '45%' }}>
+          <NibrsStackedBarDetails
+            colorMap={colorMap}
+            data={dataEntries}
+            total={totalCt}
+          />
+        </div>
+
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 NibrsStackedBar.defaultProps = {
--- src/components/NibrsStackedBarDetails.js
+++ src/components/NibrsStackedBarDetails.js
@@ -4,33 +4,20 @@ import React from 'react'
 
 const fmt = p => (p > 0.01 ? format('.0%')(p) : '<1%')
 
-const NibrsStackedBarDetails = ({
-  colorMap,
-  data,
-  onMouseOver,
-  onMouseOut,
-  selected,
-  total,
-}) =>
+const NibrsStackedBarDetails = ({ colorMap, data, total }) => (
   <ul className="list-style-none p0 m0 fs-14">
     {data.map((d, i) => {
-      const active = d.key === selected
-      const border = active ? 'border-bottom' : 'border-bottom-dashed'
-      const notLast = i + 1 !== data.length
+      const notLast = i + 1 !== data.length ? 'mb1' : ''
       return (
         <li
           key={d.key}
-          className={`${notLast
-            ? 'mb1'
-            : ''} flex items-baseline cursor-pointer ${border}`}
-          onMouseOver={onMouseOver(d.key)}
-          onMouseOut={onMouseOut}
+          className={`${notLast} flex items-baseline border-bottom-dashed`}
         >
           <span
             className="mr1"
             style={{ width: 10, height: 10, backgroundColor: colorMap(d.key) }}
           />
-          <div className={`flex flex-auto justify-between ${active && 'bold'}`}>
+          <div className="flex flex-auto justify-between">
             <span>{d.key}</span>
             <span className="monospace">{fmt(d.value / total)}</span>
           </div>
@@ -38,13 +25,11 @@ const NibrsStackedBarDetails = ({
       )
     })}
   </ul>
+)
 
 NibrsStackedBarDetails.propTypes = {
   colorMap: PropTypes.func.isRequired,
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
-  onMouseOver: PropTypes.func.isRequired,
-  onMouseOut: PropTypes.func.isRequired,
-  selected: PropTypes.string,
   total: PropTypes.number.isRequired,
 }
 
