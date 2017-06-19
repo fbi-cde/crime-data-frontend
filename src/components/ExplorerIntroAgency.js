a5adfa6e542586d5a1951e5b149b0f8a1aa8afe5
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-06-19 00:41:52 -0400

    Show county text in agency intro only if city or college
    Refs #921

--- src/components/ExplorerIntroAgency.js
+++ src/components/ExplorerIntroAgency.js
@@ -4,17 +4,22 @@ import React from 'react'
 
 import { nibrsTerm, srsTerm } from './Terms'
 
-const ExplorerIntroAgency = ({ county, name, state, type, hasNibrs }) =>
-  <p className="serif">
-    The {startCase(name)} law enforcement agency is located in
-    {' '}
-    {county && type !== 'County' && `${county} County, `}
-    {startCase(state)}. This agency reports
-    {' '}
-    {hasNibrs ? nibrsTerm : srsTerm}
-    {' '}
-    data to the FBI.
-  </p>
+const ExplorerIntroAgency = ({ county, name, state, type, hasNibrs }) => {
+  const showCounty =
+    (county && type === 'City') || type === 'University or College'
+  return (
+    <p className="serif">
+      The {startCase(name)} law enforcement agency is located in
+      {' '}
+      {showCounty && `${county} County, `}
+      {startCase(state)}. This agency reports
+      {' '}
+      {hasNibrs ? nibrsTerm : srsTerm}
+      {' '}
+      data to the FBI.
+    </p>
+  )
+}
 
 ExplorerIntroAgency.defaultProps = {
   hasNibrs: false,
