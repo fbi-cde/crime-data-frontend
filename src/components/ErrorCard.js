68f7e423826f9febf44921ba080816e7c1929fa9
Brendan Sudol <brendan.sudol@gsa.gov>
2017-06-23 14:01:12 -0400

    better error message design/content

--- src/components/ErrorCard.js
+++ src/components/ErrorCard.js
@@ -2,9 +2,12 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const ErrorCard = ({ error }) =>
-  <div className="mb2 p2 white bg-red-bright fs-14 overflow-auto">
-    <p>There was a data fetching error.</p>
-    <pre>{JSON.stringify(error, null, 2)}</pre>
+  <div className="mb2 p2 white bg-blue fs-14 overflow-auto">
+    <p className="m0">
+      <strong>Uh-oh!</strong> An error occurred during the request. Please try
+      again soon.
+    </p>
+    <pre className="display-none">{JSON.stringify(error, null, 2)}</pre>
   </div>
 
 ErrorCard.propTypes = {
