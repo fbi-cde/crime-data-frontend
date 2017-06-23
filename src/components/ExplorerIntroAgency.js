14adba5d9bbe19a5276a60904369229ceab42c6f
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2017-06-22 22:28:18 -0400

    Update agency intro text (#1037)

--- src/components/ExplorerIntroAgency.js
+++ src/components/ExplorerIntroAgency.js
@@ -1,22 +1,42 @@
+import lowerCase from 'lodash.lowercase'
 import startCase from 'lodash.startcase'
+import upperFirst from 'lodash.upperfirst'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import Term from './Term'
 import { nibrsTerm, srsTerm } from './Terms'
+import mapCrimeToGlossaryTerm from '../util/glossary'
 
-const ExplorerIntroAgency = ({ county, name, state, type, hasNibrs }) => {
+const ExplorerIntroAgency = ({
+  county,
+  crime,
+  hasNibrs,
+  name,
+  state,
+  type,
+}) => {
   const showCounty =
     (county && type === 'City') || type === 'University or College'
+  const crimeTerm = (
+    <Term id={mapCrimeToGlossaryTerm(crime)} size="sm">
+      {upperFirst(lowerCase(crime))}
+    </Term>
+  )
+
   return (
     <p className="serif">
       The {startCase(name)} law enforcement agency is located in
       {' '}
       {showCounty && `${county} County, `}
-      {startCase(state)}. This agency reports
+      {startCase(state)}.
+      {' '}
+      {crimeTerm} totals for this agency are voluntarily submitted to the
+      FBI using
       {' '}
       {hasNibrs ? nibrsTerm : srsTerm}
       {' '}
-      data to the FBI.
+      reports.
     </p>
   )
 }
