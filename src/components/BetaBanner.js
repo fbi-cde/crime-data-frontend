ddd953c0f1517f33421da745be3fe2a19477d8bb
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2017-06-28 23:07:03 -0400

    Change beta banner text (#1089)

--- src/components/BetaBanner.js
+++ src/components/BetaBanner.js
@@ -2,19 +2,13 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const BetaBanner = ({ onFeedbackClick }) =>
-  <div className="md-absolute top-0 right-0 fs-10 md-fs-16">
+  <div className="md-absolute md-col-6 top-0 right-0 fs-10 md-fs-14">
     <div className="md-mr2 p1 md-py2 md-pl3 md-pr4 md-inline-block md-rounded-bottom bg-red white">
-      <svg
-        className="mr1 align-tb xs-hide"
-        width="18"
-        height="18"
-        viewBox="0 0 24 24"
-        fill="#fff"
-      >
-        <path d="M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z" />
-      </svg>
-      <span className="bold">This site is in beta.</span>{' '}
-      Help us make it better:{' '}
+      Please note that this site is still under development and will be
+      updated
+      on a periodic basis. Feedback is welcome so that we can provide a better
+      service.
+      {' '}
       <button className="btn p0 line-height-1" onClick={onFeedbackClick}>
         Submit feedback
       </button>
