ea365731c90be03def1bb64428f0b78d9088f929
Brendan Sudol <brendan.sudol@gsa.gov>
2017-07-11 09:07:16 -0400

    webpack config hotfix

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,40 @@
-/* eslint-disable comma-dangle, no-param-reassign, no-var, quote-props  */
+/* eslint-disable */
+
 const fs = require('fs')
 const path = require('path')
 
 const autoprefixer = require('autoprefixer')
 const CompressionPlugin = require('compression-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
 const webpack = require('webpack')
 
 const env = process.env.NODE_ENV || 'development'
+
+function externals() {
+  const nodeModules = {}
+
+  fs
+    .readdirSync('node_modules')
+    .filter(x => ['.bin'].indexOf(x) === -1)
+    .forEach(mod => {
+      nodeModules[mod] = `commonjs ${mod}`
+    })
+
+  return nodeModules
+}
 
 const clientConfig = {
   entry: './src/entry.js',
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules\/(?!autotrack|dom-utils)/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
       },
       {
         test: /\.scss$/i,
@@ -32,24 +45,24 @@ const clientConfig = {
             {
               loader: 'postcss-loader',
               options: {
-                plugins: [autoprefixer('last 2 versions', '> 5%')],
-              },
+                plugins: [autoprefixer('last 2 versions', '> 5%')]
+              }
             },
             {
               loader: 'sass-loader',
               options: {
                 outputStyle: 'compressed',
-                includePaths: ['node_modules'],
-              },
-            },
-          ],
-        }),
+                includePaths: ['node_modules']
+              }
+            }
+          ]
+        })
       },
       {
         test: /\.ya*ml$/,
-        use: ['json-loader', 'yaml-loader'],
-      },
-    ],
+        use: ['json-loader', 'yaml-loader']
+      }
+    ]
   },
   plugins: [
     new CompressionPlugin({
@@ -57,15 +70,15 @@ const clientConfig = {
       algorithm: 'gzip',
       test: /\.js$/,
       threshold: 10240,
-      minRatio: 0.8,
+      minRatio: 0.8
     }),
     new ExtractTextPlugin('app.css'),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(env),
-      },
-    }),
-  ],
+        NODE_ENV: JSON.stringify(env)
+      }
+    })
+  ]
 }
 
 const serverConfig = {
@@ -73,33 +86,27 @@ const serverConfig = {
   entry: './src/server.js',
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'server.js',
+    filename: 'server.js'
   },
   target: 'node',
   node: {
-    __dirname: false,
+    __dirname: false
   },
-  externals: fs
-    .readdirSync('node_modules')
-    .filter(x => ['.bin'].indexOf(x) === -1)
-    .reduce((nodeModules, mod) => {
-      nodeModules[mod] = `commonjs ${mod}`
-      return nodeModules
-    }),
+  externals: externals(),
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules\/(?!autotrack|dom-utils)/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
       },
       {
         test: /\.ya*ml$/,
-        use: ['json-loader', 'yaml-loader'],
-      },
-    ],
+        use: ['json-loader', 'yaml-loader']
+      }
+    ]
   },
-  plugins: [new webpack.IgnorePlugin(/\.(css|less)$/)],
+  plugins: [new webpack.IgnorePlugin(/\.(css|less)$/)]
 }
 
 if (env === 'production') {
