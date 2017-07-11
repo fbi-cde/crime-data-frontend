0c93670cf39b6c0843932dbf2b414ec1c8f92d3f
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-07-11 17:20:12 -0400

    Adjust beta banner for 1024px wide screens
    To prevent the beta banner from dipping into/covering the navigation on
    displays that are between 1024px and 1060px wide I made the banner just
    a bit wider (col-7 instead of col-6)

--- src/components/BetaBanner.js
+++ src/components/BetaBanner.js
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const BetaBanner = ({ onFeedbackClick }) =>
-  <div className="lg-absolute lg-col-6 top-0 right-0 fs-10 md-fs-14">
-    <div className="lg-mr2 p1 md-py2 lg-pl3 lg-pr4 lg-inline-block lg-rounded-bottom bg-red white">
+  <div className="lg-absolute lg-col-7 top-0 right-0 fs-10 md-fs-14">
+    <div className="lg-mr2 p1 lg-py2 lg-pl3 lg-inline-block lg-rounded-bottom bg-red white">
       This site is under development and will be updated periodically. Feedback
       is welcome and will help us provide a better service.
       {' '}
