3024ff51c1d17f697fe5df6a23afc4a404708853
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-07-11 14:23:46 -0400

    Add small text changes to beta banner component
    Closes #1109

--- src/components/BetaBanner.js
+++ src/components/BetaBanner.js
@@ -2,13 +2,16 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const BetaBanner = ({ onFeedbackClick }) =>
-  <div className="md-absolute md-col-6 top-0 right-0 fs-10 md-fs-14">
-    <div className="md-mr2 p1 md-py2 md-pl3 md-pr4 md-inline-block md-rounded-bottom bg-red white">
+  <div className="lg-absolute lg-col-6 top-0 right-0 fs-10 md-fs-14">
+    <div className="lg-mr2 p1 md-py2 lg-pl3 lg-pr4 lg-inline-block lg-rounded-bottom bg-red white">
       This site is under development and will be updated periodically. Feedback
       is welcome and will help us provide a better service.
       {' '}
-      <button className="btn p0 line-height-1" onClick={onFeedbackClick}>
-        Submit feedback
+      <button
+        className="btn p0 line-height-1 underline"
+        onClick={onFeedbackClick}
+      >
+        Submit feedback.
       </button>
     </div>
   </div>
