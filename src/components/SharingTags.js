0be86b803aa113bf17b535e913b884edd57acad2
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-07-12 21:13:27 -0400

    Use CDI logo in <SharingTags />
    Also make the displayed text reflect the URL values

--- src/components/SharingTags.js
+++ src/components/SharingTags.js
@@ -3,21 +3,41 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import Helmet from 'react-helmet'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 
 import offenses from '../util/offenses'
 import { getAgency } from '../util/agencies'
 import { getPlaceInfo } from '../util/place'
 import usa from '../util/usa'
 
-const SharingTags = ({ agency, crime, place, since, until }) => {
+const clean = (val, alt) => {
+  const yr = +val
+  return yr >= 1960 && yr <= 2014 ? yr : alt
+}
+
+const SharingTags = ({ agency, crime, location, place }) => {
+  const { pathname, query } = location
+  const since = clean(query.since)
+  const until = clean(query.until, 2014)
   const placeDisplay = agency ? agency.agency_name : startCase(place)
-  const title = `${startCase(
-    crime,
-  )} offenses reported by ${placeDisplay}, ${since}–${until}`
+  const title = pathname.includes('/explorer/')
+    ? `${startCase(
+        crime,
+      )} offenses reported by ${placeDisplay}, ${since}–${until}`
+    : 'FBI Crime Data Explorer'
+
   return (
     <div>
       <Helmet>
         <meta property="og:title" content={title} />
+        <meta
+          property="og:description"
+          content="The Crime Data Explorer publishes nationwide crime data collected by the FBI in an open and accessible format. The tool allows you to view trends and download bulk data allowing you to get a better understanding of crime across the country."
+        />
+        <meta
+          property="og:image"
+          content="https://crime-data-explorer.fr.cloud.gov/img/cde-logo.png"
+        />
 
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@fbi" />
@@ -30,12 +50,10 @@ SharingTags.propTypes = {
   agency: PropTypes.oneOfType([PropTypes.bool, PropTypes.object]),
   crime: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
   place: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
-  since: PropTypes.number,
-  until: PropTypes.number,
 }
 
 const mapStateToProps = ({ agencies, filters }) => {
-  const { crime, since, until } = filters
+  const { crime } = filters
   const { place, placeType } = getPlaceInfo(filters)
   const isAgency = placeType === 'agency'
   const agency = isAgency && getAgency(agencies, place)
@@ -46,9 +64,7 @@ const mapStateToProps = ({ agencies, filters }) => {
     agency,
     crime: isKnownCrime ? crime : false,
     place: isKnownPlace ? place : false,
-    since,
-    until,
   }
 }
 
-export default connect(mapStateToProps)(SharingTags)
+export default withRouter(connect(mapStateToProps)(SharingTags))
