31ff167df7e9aaf65892d9519d8c3afacf142a2f
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-07-25 20:01:44 -0400

    Add count/percent toggle to sex demo stacked bar chart

--- src/components/NibrsStackedBar.js
+++ src/components/NibrsStackedBar.js
@@ -1,72 +1,121 @@
 import { entries } from 'd3-collection'
 import { scaleBand, scaleLinear, scaleOrdinal } from 'd3-scale'
 import { stack, stackOrderReverse } from 'd3-shape'
+import pluralize from 'pluralize'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import NibrsCountPercentToggle from './NibrsCountPercentToggle'
 import NibrsStackedBarDetails from './NibrsStackedBarDetails'
+import { formatNum } from '../util/formats'
 
-const NibrsStackedBar = ({ colors, keys, data, margin, size, title }) => {
-  const height = size.height - margin.top - margin.bottom
-  const width = size.width - margin.left - margin.right
-  const totalCt = data.reduce((a, b) => a + +b.count, 0)
-  const x = scaleBand().domain([null]).rangeRound([0, width]).padding(0.4)
-  const y = scaleLinear().domain([0, totalCt]).rangeRound([height, 0]).nice()
+class NibrsStackedBar extends React.Component {
+  state = { isCounts: false }
 
-  const colorMap = scaleOrdinal()
-    .domain(keys || data.map(d => d.key).sort())
-    .range(colors)
+  render() {
+    const {
+      colors,
+      id,
+      keys,
+      data,
+      margin,
+      noun,
+      sentenceStart,
+      size,
+      title,
+    } = this.props
+    const { isCounts } = this.state
+    const height = size.height - margin.top - margin.bottom
+    const width = size.width - margin.left - margin.right
+    const totalCt = data.reduce((a, b) => a + +b.count, 0)
+    const x = scaleBand().domain([null]).rangeRound([0, width]).padding(0.4)
+    const y = scaleLinear().domain([0, totalCt]).rangeRound([height, 0]).nice()
 
-  const lookup = Object.assign(...data.map(d => ({ [d.key]: +d.count })))
-  const dataClean = keys
-    ? Object.assign(...keys.map(k => ({ [k]: lookup[k] || 0 })))
-    : { ...lookup }
+    const colorMap = scaleOrdinal()
+      .domain(keys || data.map(d => d.key).sort())
+      .range(colors)
 
-  const stackGen = stack().keys(Object.keys(dataClean)).order(stackOrderReverse)
+    const lookup = Object.assign(...data.map(d => ({ [d.key]: +d.count })))
+    const dataClean = keys
+      ? Object.assign(...keys.map(k => ({ [k]: lookup[k] || 0 })))
+      : { ...lookup }
 
-  const dataStacked = stackGen([dataClean])
-  const dataEntries = entries(dataClean)
+    const stackGen = stack()
+      .keys(Object.keys(dataClean))
+      .order(stackOrderReverse)
 
-  return (
-    <div className="mb2 pb2 border-bottom border-blue-light">
-      <div className="mb2 blue bold">{title}</div>
-      <div className="flex flex-wrap items-end mxn1 mb2">
-        <div className="col px1" style={{ width: '55%' }}>
-          <svg
-            className="block"
-            preserveAspectRatio="xMidYMid"
-            viewBox={`0 0 ${size.width} ${size.height}`}
-            style={{ width: '100%', height: '100%' }}
-          >
-            <g transform={`translate(${margin.left}, ${margin.top})`}>
-              {dataStacked.map(d =>
-                <g key={d.key} fill={colorMap(d.key)}>
-                  <rect
-                    x={x(null)}
-                    y={y(d[0][1])}
-                    height={y(d[0][0]) - y(d[0][1])}
-                    width={x.bandwidth()}
-                  />
-                </g>,
-              )}
-              <g className="axis" transform={`translate(0, ${height})`}>
-                <line x2={width} strokeWidth="1" />
+    const dataStacked = stackGen([dataClean])
+    const dataEntries = entries(dataClean)
+
+    const totalCount = dataEntries.reduce(
+      (total, next) => total + next.value,
+      0,
+    )
+
+    return (
+      <div className="mb2 pb2 border-bottom border-blue-light">
+        <div className="clearfix">
+          <div className="left">
+            <div className="mb2 blue bold">
+              {title}
+            </div>
+          </div>
+          <div className="right">
+            <NibrsCountPercentToggle
+              ariaControls={id}
+              isCounts={isCounts}
+              showCounts={() => {
+                this.setState({ isCounts: true })
+              }}
+              showPercents={() => {
+                this.setState({ isCounts: false })
+              }}
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap items-end mxn1 mb2">
+          <div className="col px1" style={{ width: '55%' }}>
+            <svg
+              className="block"
+              preserveAspectRatio="xMidYMid"
+              viewBox={`0 0 ${size.width} ${size.height}`}
+              style={{ width: '100%', height: '100%' }}
+            >
+              <g transform={`translate(${margin.left}, ${margin.top})`}>
+                {dataStacked.map(d =>
+                  <g key={d.key} fill={colorMap(d.key)}>
+                    <rect
+                      x={x(null)}
+                      y={y(d[0][1])}
+                      height={y(d[0][0]) - y(d[0][1])}
+                      width={x.bandwidth()}
+                    />
+                  </g>,
+                )}
+                <g className="axis" transform={`translate(0, ${height})`}>
+                  <line x2={width} strokeWidth="1" />
+                </g>
               </g>
-            </g>
-          </svg>
-        </div>
+            </svg>
+          </div>
 
-        <div className="col px1" style={{ width: '45%' }}>
-          <NibrsStackedBarDetails
-            colorMap={colorMap}
-            data={dataEntries}
-            total={totalCt}
-          />
+          <div className="col px1" id={id} style={{ width: '45%' }}>
+            <NibrsStackedBarDetails
+              colorMap={colorMap}
+              data={dataEntries}
+              isCounts={isCounts}
+              total={totalCt}
+            />
+          </div>
         </div>
-
+        <div className="mt-tiny fs-14 mb3">
+          {sentenceStart} was reported for{' '}
+          <span className="bold red">{formatNum(totalCount)}</span>{' '}
+          {pluralize(noun, totalCount)}.
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 NibrsStackedBar.defaultProps = {
--- src/components/NibrsStackedBarDetails.js
+++ src/components/NibrsStackedBarDetails.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import { formatPerc as fmt } from '../util/formats'
+import { formatNum, formatPerc } from '../util/formats'
 
-const NibrsStackedBarDetails = ({ colorMap, data, total }) =>
+const NibrsStackedBarDetails = ({ colorMap, data, isCounts, total }) =>
   <ul className="list-style-none p0 m0 fs-14">
     {data.map((d, i) => {
       const notLast = i + 1 !== data.length ? 'mb1' : ''
@@ -17,8 +17,12 @@ const NibrsStackedBarDetails = ({ colorMap, data, total }) =>
             style={{ width: 10, height: 10, backgroundColor: colorMap(d.key) }}
           />
           <div className="flex flex-auto justify-between">
-            <span>{d.key}</span>
-            <span className="monospace">{fmt(d.value / total)}</span>
+            <span>
+              {d.key}
+            </span>
+            <span className="bold monospace">
+              {isCounts ? formatNum(d.value) : formatPerc(d.value / total)}
+            </span>
           </div>
         </li>
       )
@@ -28,7 +32,12 @@ const NibrsStackedBarDetails = ({ colorMap, data, total }) =>
 NibrsStackedBarDetails.propTypes = {
   colorMap: PropTypes.func.isRequired,
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  isCounts: PropTypes.bool,
   total: PropTypes.number.isRequired,
+}
+
+NibrsStackedBarDetails.defaultProps = {
+  isCounts: false,
 }
 
 export default NibrsStackedBarDetails
