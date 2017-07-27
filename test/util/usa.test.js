c2647ee298b88e46f7fe57b2f994abbc32b5ecef
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2017-07-27 16:10:29 -0400

    Change underlying USA data to include slug and display names
    So that we don't have to deal with slugify-ing and lowercasing and
    startcasing all over the place for place names (which is problematic
    when you have display names like "Washington, DC" I updated the
    underlying data structure to be an array of objects that contain all
    the US state (and DC) data we might need for the API, the URLs and the
    UI

--- test/util/usa.test.js
+++ test/util/usa.test.js
@@ -1,19 +1,19 @@
 /* eslint no-undef: 0 */
 
-import lookup, { data as states } from '../../src/util/usa'
+import lookup from '../../src/util/usa'
 
 describe('usa utility', () => {
   describe('with an abbreviation', () => {
     it('should return the proper state', () => {
       const expected = 'california'
       const actual = lookup('ca')
-      expect(actual).toEqual(expected)
+      expect(actual.slug).toEqual(expected)
     })
 
     it('should work with uppercase abbreviations', () => {
       const expected = 'california'
       const actual = lookup('CA')
-      expect(actual).toEqual(expected)
+      expect(actual.slug).toEqual(expected)
     })
   })
 
@@ -21,7 +21,7 @@ describe('usa utility', () => {
     it('should return the proper abbreviation', () => {
       const expected = 'ca'
       const actual = lookup('california')
-      expect(actual).toEqual(expected)
+      expect(actual.id).toEqual(expected)
     })
   })
 })
