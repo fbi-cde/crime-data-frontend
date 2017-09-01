de76ed0b476b99996a8e1391c62becdaf708197e
Jacob Wentz <jpwentz@ws05.ucr.development.cjis.gov>
2017-09-01 10:14:03 -0400

    Adds Display of when a State Started Reporting NIBRS Data

--- src/components/explorer/ExplorerIntroState.js
+++ src/components/explorer/ExplorerIntroState.js
@@ -1,6 +1,7 @@
 import lowerCase from 'lodash.lowercase'
 import upperFirst from 'lodash.upperfirst'
 import React from 'react'
+import { min } from 'd3-array'
 
 import Term from '../Term'
 import { EstimatedTerm, NibrsTerm, SrsTerm } from '../Terms'
@@ -25,6 +26,28 @@ const ExplorerIntroState = ({ crime, place, participation, until }) => {
   const isArson = crime === 'arson'
   const { nibrs, srs } = ucrParticipationLookup(place)
   const untilUcr = participation.find(p => p.year === until)
+
+  let reportYrStr = ''
+  if (nibrs) {
+    reportYrStr = (
+      <span>
+        {lookupUsa(place).display} began submitting <NibrsTerm /> data reports
+        in {nibrs['initial-year']}
+      </span>
+    )
+  }
+  console.log('reportYrStr:', reportYrStr)
+  /*
+  console.log('participation:', participation)
+  const years = participation
+    .filter(p => p.nibrs_participating_agencies > 0)
+    .map(p => p.year)
+  for (let j = 1; j < years.length; j++) {
+    console.log('j:', years[j])
+  }
+  const minYr = min(years)
+  console.log('Min Yr:', minYr)
+*/
   const reportTerms = getReportTerms({ nibrs, srs, hybrid: nibrs && srs })
   const crimeTerm = (
     <Term id={mapCrimeToGlossaryTerm(crime)}>
@@ -38,7 +61,8 @@ const ExplorerIntroState = ({ crime, place, participation, until }) => {
         ? <div>
             <p className="serif">
               {crimeTerm} rates for {lookupUsa(place).display} are derived from{' '}
-              {reportTerms} reports voluntarily submitted to the FBI.
+              {reportTerms} reports voluntarily submitted to the FBI.{' '}
+              {reportYrStr} .
             </p>
             <p className="serif">
               In {highlight(until)}
