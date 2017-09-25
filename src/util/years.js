c8ab2977cd53f018c6de4dd011e422e56de126d3
Jacob Wentz <jpwentz@ws05.ucr.development.cjis.gov>
2017-09-25 09:03:24 -0400

    Update to 2016

--- src/util/years.js
+++ src/util/years.js
@@ -3,5 +3,5 @@ import range from 'lodash.range'
 export const rangeYears = (since, until) => range(since, until + 1)
 
 export const MIN_YEAR = 1995
-export const MAX_YEAR = 2015
+export const MAX_YEAR = 2016
 export const YEAR_RANGE = rangeYears(MIN_YEAR, MAX_YEAR)
