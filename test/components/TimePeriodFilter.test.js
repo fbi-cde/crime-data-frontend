c0d8d9a94d63653d2672aaa54f4dbbc2e621e10a
Jacob Wentz <jpwentz@ws05.ucr.development.cjis.gov>
2017-10-12 14:45:27 -0400

    Adds Time Filter Requirement Back for Release

--- test/components/TimePeriodFilter.test.js
+++ test/components/TimePeriodFilter.test.js
@@ -33,7 +33,7 @@ describe('TimePeriodFilter', () => {
       expect(props.onChange.getCall(0)).not.toBeNull()
     })
 
-/*
+
     it('should not allow time ranges of less than ten years', () => {
       const props = {
         ariaControls: '',
@@ -63,7 +63,7 @@ describe('TimePeriodFilter', () => {
         'The beginning year must be earlier than the end year',
       )
     })
-    */
+
   })
 
   describe('setError()', () => {
