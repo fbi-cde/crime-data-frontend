71ce21918a4aa26ececab43bae81e304f2bfd005
Jacob Wentz <jpwentz@ws05.ucr.development.cjis.gov>
2017-10-20 08:57:40 -0400

    Adds Banner as Footnotes for NIBRS Errors

--- src/components/nibrs/NibrsIntro.js
+++ src/components/nibrs/NibrsIntro.js
@@ -29,16 +29,6 @@ const NibrsIntro = ({
           {crime} {pluralize(noun, totalCount)} to the FBI between{' '}
           {highlight(nibrsFirstYear)} and {highlight(until)}.
         </p>
-        <p className="fs-12 serif italic">
-          The complexity of NIBRS data presents unique impediments to interconnecting
-          all facets of the information collected. In instances of multiple
-          offenders, for example, the Crime Data Explorer currently links an offender
-          to only one offense—the first listed. The same is true for incidents
-          involving multiple victims. The Uniform Crime Reporting Program is
-          working hard to improve these specific functions within the Crime Data
-          Explorer so that presentations in the coming months will fully encompass
-          all aspects of the NIBRS data.
-        </p>
       </p>
     )
   }
@@ -54,16 +44,6 @@ const NibrsIntro = ({
         between {highlight(nibrsFirstYear)} and {highlight(until)} by{' '}
         {highlight(agencyCt)} law enforcement {pluralize('agency', agencyCt)}{' '}
         reporting {nibrsTerm} data.
-      </p>
-      <p className="fs-12 serif italic">
-        The complexity of NIBRS data presents unique impediments to interconnecting
-        all facets of the information collected. In instances of multiple
-        offenders, for example, the Crime Data Explorer currently links an offender
-        to only one offense—the first listed. The same is true for incidents
-        involving multiple victims. The Uniform Crime Reporting Program is
-        working hard to improve these specific functions within the Crime Data
-        Explorer so that presentations in the coming months will fully encompass
-        all aspects of the NIBRS data.
       </p>
     </p>
   )
