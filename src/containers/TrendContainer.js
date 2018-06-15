e8d4e90b2f51429572b9201e33120de03b3809bf
Jacob Wentz <jpwentz@ws05.ucr.development.cjis.gov>
2018-06-15 09:16:29 -0400

    Fix to TrendChart Downloads

--- src/containers/TrendContainer.js
+++ src/containers/TrendContainer.js
@@ -24,27 +24,39 @@ class TrendContainer extends React.Component {
   }
 
   getContent = ({ crime, filters, places, summaries, placeName }) => {
+    console.log(
+      'crime, filters, places, summaries, placeName:',
+      crime,
+      filters,
+      places,
+      summaries,
+      placeName
+    )
     const { loading, error } = summaries
     const { yearSelected } = this.state
 
     if (loading) return <Loading />
     if (error) return <ErrorCard error={error} />
 
-
     const offenses =
       crime === 'rape' ? ['rape-legacy', 'rape-revised'] : [crime]
 
-    const filteredByYear = filterByYear(summaries.data, filters.since, filters.until)
+    const filteredByYear = filterByYear(
+      summaries.data,
+      filters.since,
+      filters.until
+    )
     const data = combinePlaces(filteredByYear, offenses)
     if (!data || data.length === 0) return <NoData />
 
-    const fname = `${filters.place}-${filters.pageType}-${filters.since}-${filters.until}`
+    const fname = `${filters.place}-${crime}-${filters.since}-${filters.until}`
+
     const title =
-      `Reported ${pluralize(filters.pageType)} in ` +
+      `Reported ${pluralize(crime)} in ` +
       `${placeName}, ${filters.since}-${filters.until}`
 
-    const readme = generateCrimeReadme({ crime: filters.pageType, title })
-    const crimeNorm = filters.pageType === 'rape' ? 'rape-legacy' : filters.pageType
+    const readme = generateCrimeReadme({ crime, title })
+    const crimeNorm = crime === 'rape' ? 'rape-legacy' : crime
     const dlData = data.map(d => {
       const placeData = places.map(p => ({ [p]: { ...d[p][crimeNorm] } }))
       return { year: d.year, ...Object.assign(...placeData) }
@@ -52,7 +64,7 @@ class TrendContainer extends React.Component {
 
     const download = [
       { content: readme, filename: 'README.md' },
-      { data: dlData, filename: `${fname}.csv` },
+      { data: dlData, filename: `${fname}.csv` }
     ]
 
     return (
@@ -80,29 +92,26 @@ class TrendContainer extends React.Component {
   }
 
   render() {
-    const {
-      places,
-      summaries,
-      region,
-      states,
-      filters,
-    } = this.props
+    const { places, summaries, region, states, filters } = this.props
     const placeName = lookupDisplayName(filters, region.regions, states.states)
     const isReady = !summaries.loading
 
     let otherCrimes = []
     if (filters.pageType === 'violent-crime') {
-     otherCrimes = ['homicide', 'rape', 'robbery', 'aggravated-assault']
-   } else if (filters.pageType === 'property-crime') {
-     otherCrimes = ['arson', 'burglary', 'larceny', 'motor-vehicle-theft']
-}
+      otherCrimes = ['homicide', 'rape', 'robbery', 'aggravated-assault']
+    } else if (filters.pageType === 'property-crime') {
+      otherCrimes = ['arson', 'burglary', 'larceny', 'motor-vehicle-theft']
+    }
     const crime = filters.pageType
     const placeType = filters.placeType
     return (
       <div className="mb7">
         <div className="mb2 p2 sm-p4 bg-white border-top border-blue border-w8">
           <h2 className="mt0 mb2 sm-mb4 fs-24 sm-fs-28 sans-serif">
-            {startCase(filters.pageType)} rate in {placeType === 'region' ? 'the' : ''} {placeName}, {filters.since}-{filters.until}
+            {startCase(filters.pageType)} rate in{' '}
+            {placeType === 'region' ? 'the' : ''} {placeName}, {filters.since}-{
+              filters.until
+            }
           </h2>
           <div className="bg-white">
             {this.getContent({ crime, filters, places, summaries, placeName })}
@@ -131,8 +140,14 @@ class TrendContainer extends React.Component {
             })}
           </div>
         </div>
-        {isReady &&
-          <TrendSourceText crime={filters.pageType} place={filters.place} placeType={filters.placeType} placeName={placeName} />}
+        {isReady && (
+          <TrendSourceText
+            crime={filters.pageType}
+            place={filters.place}
+            placeType={filters.placeType}
+            placeName={placeName}
+          />
+        )}
       </div>
     )
   }
@@ -141,10 +156,10 @@ TrendContainer.propTypes = {
   places: PropTypes.arrayOf(PropTypes.string),
   summaries: PropTypes.shape({
     data: PropTypes.object,
-    loading: PropTypes.boolean,
+    loading: PropTypes.boolean
   }).isRequired,
   region: PropTypes.object,
-  states: PropTypes.object,
+  states: PropTypes.object
 }
 
 export const mapStateToProps = ({ filters, summaries, region, states }) => {
@@ -159,7 +174,7 @@ export const mapStateToProps = ({ filters, summaries, region, states }) => {
     ...getPlaceInfo(filters),
     region,
     states,
-    filters,
+    filters
   }
 }
 
